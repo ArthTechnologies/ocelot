ada2ae2afb84aea55f77468e9314af9d55c72855
DiamonC <88857034+DiamonC@users.noreply.github.com>
2023-01-28 18:36:31 -0600

    fixed structory image, removed placeholder server

--- src/lib/components/ui/ChooseVersion.svelte
+++ src/lib/components/ui/ChooseVersion.svelte
@@ -64,7 +64,7 @@ cd                 }
     <div class="flex justify-between">
         <h3 class="font-bold text-lg">Versions</h3>
     <div class="modal-action">
-        <label for="versions" class="btn btn-circle btn-sm btn-ghost"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></label>
+        <label for="versions" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
       </div>
     </div>
 
