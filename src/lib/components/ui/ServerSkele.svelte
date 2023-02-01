b3d8d63c8694b8ac22540f43ca2852e8206c10a3
DiamonC <88857034+DiamonC@users.noreply.github.com>
2023-01-31 20:26:01 -0600

    fixed starting button

--- src/lib/components/ui/ServerSkele.svelte
+++ src/lib/components/ui/ServerSkele.svelte
@@ -1,35 +1,32 @@
 <div class="m-3">
-    <div class="card w-64 h-40 bg-base-100 shadow-xl image-full ">
-      <div class="card-body animate-pulse ">
-        <h2 class="card-title">        <div class="h-2 bg-base-100 rounded w-16 mt-4"></div></h2>
-  
-          <div class="h-2 bg-base-100 rounded w-24 mt-2"></div>
-   
-        <!-- <div class="card-actions justify-beginning" /> -->
-        <div class="card-actions mt-2">
-          <!-- placeholder for now? -->
-          <div class="grow space-x-1.5 flex">
-            <button
-            
-              type="submit"
-              class="btn btn-disabled bg-base-100 btn-sm h-9 w-14"></button
-            >
-                                <button
-            
-              type="submit"
-              class="btn btn-disabled bg-base-100 btn-sm h-9 w-16"></button
-            >
-                                <button
-            
-              type="submit"
-              class="btn btn-disabled bg-base-100 btn-sm h-9 w-14"></button
-            >
-          </div>
+  <div class="card w-[20rem] h-40 bg-base-100 shadow-xl image-full ">
+    <div class="card-body animate-pulse ">
+      <h2 class="card-title">
+        <div class="h-2 bg-base-100 rounded w-16 mt-4" />
+      </h2>
 
-            <div class="badge w-12 bg-base-100  right-2 top-4 absolute"></div>
+      <div class="h-2 bg-base-100 rounded w-24 mt-2" />
 
+      <!-- <div class="card-actions justify-beginning" /> -->
+      <div class="card-actions mt-2">
+        <!-- placeholder for now? -->
+        <div class="grow space-x-1.5 flex">
+          <button
+            type="submit"
+            class="btn btn-disabled bg-base-100 btn-sm h-9 w-14"
+          />
+          <button
+            type="submit"
+            class="btn btn-disabled bg-base-100 btn-sm h-9 w-16"
+          />
+          <button
+            type="submit"
+            class="btn btn-disabled bg-base-100 btn-sm h-9 w-14"
+          />
         </div>
-  
+
+        <div class="badge w-12 bg-base-100  right-2 top-4 absolute" />
       </div>
     </div>
   </div>
+</div>
