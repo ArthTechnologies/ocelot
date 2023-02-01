a041bd1bcf1220667e5f34c70f95df87500c4dea
DiamonC <88857034+DiamonC@users.noreply.github.com>
2023-02-01 14:53:24 -0600

    added restarting ui

--- src/routes/(login)/server/[slug]/+page.svelte
+++ src/routes/(login)/server/[slug]/+page.svelte
@@ -27,7 +27,7 @@
   let lock = false;
   let s = "Paper";
   let v = "latest";
-
+  let restarting = false;
   let email: string = "";
   let state = "false";
   if (browser) {
@@ -75,6 +75,10 @@
     getServer(id).then((response) => {
       //set state to response
       state = response.state;
+      if (restarting && state == "starting") {
+        restarting = false;
+        lock = false;
+      }
       console.log(id + "'s state is " + state);
     });
   }
@@ -83,6 +87,8 @@
     if (!lock) {
       if (state == "true") {
         changeServerState("restart", id, email);
+
+        restarting = true;
       } else if (state == "false") {
         changeServerState("start", id, email);
       }
@@ -189,7 +195,7 @@
     </div>
     <!-- TODO: these should be on the right, add an if for not reaching the backend -->
     <div class="space-x-2 space-y-2">
-      {#if state == "true"}
+      {#if state == "true" && !restarting}
         <button on:click={start} class="btn btn-success"
           ><svg
             xmlns="http://www.w3.org/2000/svg"
@@ -229,6 +235,64 @@
             /></svg
           >{$t("button.stop2")}</button
         >
+        {:else if restarting}
+        <button class="btn btn-success"
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-loader animate-spin"
+            ><line x1="12" y1="2" x2="12" y2="6" /><line
+              x1="12"
+              y1="18"
+              x2="12"
+              y2="22"
+            /><line x1="4.93" y1="4.93" x2="7.76" y2="7.76" /><line
+              x1="16.24"
+              y1="16.24"
+              x2="19.07"
+              y2="19.07"
+            /><line x1="2" y1="12" x2="6" y2="12" /><line
+              x1="18"
+              y1="12"
+              x2="22"
+              y2="12"
+            /><line x1="4.93" y1="19.07" x2="7.76" y2="16.24" /><line
+              x1="16.24"
+              y1="7.76"
+              x2="19.07"
+              y2="4.93"
+            /></svg
+          >
+          restarting</button
+        >
+        
+        <button  class="btn btn-disabled"
+        ><svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-stop-circle"
+          ><circle cx="12" cy="12" r="10" /><rect
+            x="9"
+            y="9"
+            width="6"
+            height="6"
+          /></svg
+        >stop</button
+      >
       {:else if state == "false"}
         <button on:click={start} class="btn btn-success"
           ><svg
