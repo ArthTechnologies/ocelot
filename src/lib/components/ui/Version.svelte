2d60c810290c72fdd775a37561e1cf503f7ed60b
DiamonC <88857034+DiamonC@users.noreply.github.com>
2023-01-31 21:39:36 -0600

    made adding a version less confusing

--- src/lib/components/ui/Version.svelte
+++ src/lib/components/ui/Version.svelte
@@ -37,7 +37,28 @@
         </div>
         <div class="flex place-items-center space-x-2">
             <p>{time}</p>
-            <button on:click={submit} class="btn btn-ghost btn-circle btn-sm"
+
+            <label
+                on:click={submit}
+                class="btn btn-circle btn-ghost swap swap-rotate"
+            >
+                <input type="checkbox" /><svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="feather feather-plus swap-off"
+                    ><line x1="12" y1="5" x2="12" y2="19" /><line
+                        x1="5"
+                        y1="12"
+                        x2="19"
+                        y2="12"
+                    /></svg
                 ><svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="24"
@@ -48,9 +69,9 @@
                     stroke-width="2"
                     stroke-linecap="round"
                     stroke-linejoin="round"
-                    class="feather feather-check"
+                    class="feather feather-check swap-on"
                     ><polyline points="20 6 9 17 4 12" /></svg
-                ></button
+                ></label
             >
         </div>
     </div>
