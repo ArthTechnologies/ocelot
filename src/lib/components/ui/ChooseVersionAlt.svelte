f34130e84ae42ee5520aa6294b6d940f8ca5b015
DiamonC <88857034+DiamonC@users.noreply.github.com>
2023-03-18 22:50:42 -0500

    changed some wording, now shows # of servers in /pay

--- src/lib/components/ui/ChooseVersionAlt.svelte
+++ src/lib/components/ui/ChooseVersionAlt.svelte
@@ -49,7 +49,7 @@
       //if it's still blank, add a message saying that there are no versions for this plugin
       if (document.getElementById("list").innerHTML == "") {
         document.getElementById("list").innerHTML =
-          "<p class='text-center'>There are no versions for this plugin.</p>";
+          "<p class='text-center'>This plugin doesn't support your Minecraft version currently.</p>";
       }
     });
   }
