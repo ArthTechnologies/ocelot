0a909b13ecdb5f7e9087f3f2b7d1b4c0f22f60e0
diamonc <diamonc@proton.me>
2023-03-20 17:43:33 -0500

    added rss system, disabled analytics until new privacy policy goes into effect

--- run.js
+++ run.js
@@ -24,24 +24,25 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on Port: ${port}`));
 
 // put blog posts in rss
-let dir = fs.readdirSync("./files/posts")
+let posts = JSON.parse(fs.readFileSync("./files/posts/index.json").toString());
 let rss = fs.readFileSync("arthblog.rss").toString();
-let rssp1 = rss.split("<item>")[0]
-let rssp2 = rss.split("<item>")[1,dir.length-1]
-for (i in dir) {
-let item = `<item>
-<title>Introducing Arth Panel</title>
+let rssp1 = rss.split("<!-- Posts -->")[0]
+let rssp2 = rss.split("<!-- Posts -->")[1,posts.length-1]
+let items = [];
+for (i in posts) {
+items.push(`<item>
+<title>`+posts[i].title+`</title>
 <description>
-Your gateway to running your own Minecraft servers.
+`+posts[i].desc+`
 </description>
 
-<link>https://arthmc.xyz/blog/introducing-arth-panel</link>
-<guid isPermaLink="true">https://arthmc.xyz/blog/introducing-arth-panel</guid>
-<pubDate>Sun, 08 Jan 2023 12:00:00 +0000</pubDate>
+<link>https://arthmc.xyz/blog/`+posts[i].slug+`</link>
+<guid isPermaLink="true">https://arthmc.xyz/blog/`+posts[i].slug+`</guid>
 
-
-</item>`
+</item>`);
 }
+fs.writeFileSync("arthblog.rss", rssp1+"<!-- Posts -->\n"+items.join("")+rssp2);
+
 
 app.use((err, req, res, next) => {
   switch (err.message) {
