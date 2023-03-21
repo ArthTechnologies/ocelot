9a510063acef903ed6ba9232c8635a67cd984856
diamonc <diamonc@proton.me>
2023-03-20 20:56:39 -0500

    made images a little smaller in view

--- routes/view.js
+++ routes/view.js
@@ -3,16 +3,17 @@ const Router = express.Router();
 const fs = require("fs");
 
 Router.get("/post/:file", (req, res) => {
+  let text = fs
+    .readFileSync("./files/posts/" + req.params.file + ".md")
+    .toString();
 
-    let text = fs.readFileSync("./files/posts/"+req.params.file+".md").toString();
+  //converts markdown to html using marked
+  const marked = require("marked");
+  text = marked.parse(text.split("\n").slice(6).join("\n"));
 
-    //converts markdown to html using marked
-    const marked = require("marked");
-    text = marked.parse(text.split("\n").slice(6).join("\n"));
-
-    //make sure images arent too big
-    text = text.replace(/<img/g, "<img style='max-width:100%'");
-    res.send(text)
+  //make sure images arent too big
+  text = text.replace(/<img/g, "<img style='max-width:80% max-height:80%'");
+  res.send(text);
 });
 
 module.exports = Router;
