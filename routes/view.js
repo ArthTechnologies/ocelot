0c522f16be735185c48c9982a296276ba37fea72
diamonc <diamonc@proton.me>
2023-03-20 20:33:43 -0500

    made images smaller

--- routes/view.js
+++ routes/view.js
@@ -5,11 +5,13 @@ const fs = require("fs");
 Router.get("/post/:file", (req, res) => {
 
     let text = fs.readFileSync("./files/posts/"+req.params.file+".md").toString();
-    
+
     //converts markdown to html using marked
     const marked = require("marked");
     text = marked.parse(text.split("\n").slice(6).join("\n"));
 
+    //make sure images arent too big
+    text = text.replace(/<img/g, "<img style='max-width:40%'");
     res.send(text)
 });
 
