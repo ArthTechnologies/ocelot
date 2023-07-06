6ff6fe900c5f6c22b234b8e025eae6b4bbf59029
DiamonC <88857034+DiamonC@users.noreply.github.com>
2023-07-06 17:13:51 -0500

    changed look of deleteserver warning

--- src/lib/components/ui/DeleteServer.svelte
+++ src/lib/components/ui/DeleteServer.svelte
@@ -2,32 +2,13 @@
   import { deleteServer } from "$lib/scripts/req";
   import { t } from "$lib/scripts/i18n";
   import { browser } from "$app/environment";
+  import World from "./World.svelte";
   let id = -1;
   if (browser) {
     id = localStorage.getItem("serverID");
   }
   function del() {
     deleteServer(id);
-  }
-  function download() {
-    return fetch("https://api.arthmc.xyz/server/" + id + "/world", {
-      method: "GET",
-      headers: {
-        token: localStorage.getItem("token"),
-        email: localStorage.getItem("accountEmail"),
-      },
-    })
-      .then((res) => res.text())
-      .then((input: string) => {
-        console.log(input);
-        //download the file
-        const element = document.createElement("a");
-        const file = new Blob([input], { type: "text/plain" });
-        element.href = URL.createObjectURL(file);
-        element.download = "world.zip";
-        document.body.appendChild(element); // Required for this to work in FireFox
-        element.click();
-      });
   }
 </script>
 
@@ -63,12 +44,27 @@
       >✕</label
     >
     <h3 class="text-lg font-bold">Do you want to delete this server?</h3>
-    <p class="py-4">
-      Your server will be instantly deleted. This cannot be undone.
-    </p>
-    <button class="btn btn-primary" on:click={download}
-      >Download World File</button
+    <div
+      class="bg-warning w-86 h-12 rounded-lg text-black p-2 flex items-center mb-6 space-x-2 mt-2"
     >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="stroke-current shrink-0 h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        ><path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+        /></svg
+      >
+      <span class="text-sm"
+        >Warning: Your server will be instantly deleted. This cannot be undone.
+        Consider downloading your world first.</span
+      >
+    </div>
+
     <a href="/"
       ><button class="btn btn-error" on:click={del}
         >{$t("button.delete")}</button
