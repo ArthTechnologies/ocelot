1c394a54a5aa702fdeca74badb2819fc91b6c3e2
DiamonC <88857034+DiamonC@users.noreply.github.com>
2023-07-06 22:16:43 -0500

    added stopping indicator

--- src/routes/(login)/server/[slug]/+page.svelte
+++ src/routes/(login)/server/[slug]/+page.svelte
@@ -185,6 +185,7 @@
 
       //set state to response
       state = response.state;
+      console.log(state);
       if (restarting && state == "starting") {
         restarting = false;
         console.log("unlocking");
@@ -556,6 +557,60 @@
               height="6"
             /></svg
           >Stop</button
+        >
+      {:else if state == "stopping"}
+        <button class="btn btn-disabled"
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-play-circle mr-1.5"
+            ><circle cx="12" cy="12" r="10" /><polygon
+              points="10 8 16 12 10 16 10 8"
+            /></svg
+          >{$t("button.start")}</button
+        >
+        <button class="btn btn-error"
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-loader animate-spin mr-1.5"
+            ><line x1="12" y1="2" x2="12" y2="6" /><line
+              x1="12"
+              y1="18"
+              x2="12"
+              y2="22"
+            /><line x1="4.93" y1="4.93" x2="7.76" y2="7.76" /><line
+              x1="16.24"
+              y1="16.24"
+              x2="19.07"
+              y2="19.07"
+            /><line x1="2" y1="12" x2="6" y2="12" /><line
+              x1="18"
+              y1="12"
+              x2="22"
+              y2="12"
+            /><line x1="4.93" y1="19.07" x2="7.76" y2="16.24" /><line
+              x1="16.24"
+              y1="7.76"
+              x2="19.07"
+              y2="4.93"
+            /></svg
+          >
+          Stopping</button
         >
       {/if}
     </div>
