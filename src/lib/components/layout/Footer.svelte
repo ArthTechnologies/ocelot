c0b7cf8789db28976a896a6d35c1b0cd5ccd4d94
DiamonC <88857034+DiamonC@users.noreply.github.com>
2023-07-24 22:52:01 -0400

    fixed visual bugs with support modal

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -52,7 +52,7 @@
       Join Servers
     </a>
     <h3 class="text-lg font-bold mt-2 mb-1">Contact</h3>
-    <div class="md:flex space-x-2 space-y-2">
+    <div class="md:flex md:space-x-2 space-y-2 md:space-y-0">
       <h3
         class="text-lg font-bold bg-base-200 w-[17rem] rounded-lg px-3 py-2 flex items-center"
       >
@@ -75,7 +75,7 @@
 
       <div class="flex">
         <a
-          class="btn z-50 -ml-2"
+          class="btn z-50"
           href="https://discord.gg/gCafNVA43d"
           target="_blank"
           rel="noreferrer"
