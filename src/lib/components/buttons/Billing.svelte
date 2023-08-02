dc36022a49cbebd3239e7ce6f1f78069e6a2ef87
DiamonC <88857034+DiamonC@users.noreply.github.com>
2023-08-02 10:14:59 -0500

    rearranged navbar billing icon

--- src/lib/components/buttons/Billing.svelte
+++ src/lib/components/buttons/Billing.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
-  import { CreditCard, Landmark, ShoppingCart } from "lucide-svelte";
+  import {
+    CreditCard,
+    Landmark,
+    ShoppingBag,
+    ShoppingCart,
+    Wallet,
+    Wallet2,
+    WalletCards,
+  } from "lucide-svelte";
 </script>
 
-<a href="/pay" id="pay" class="nav btn btn-ghost btn-circle"><Landmark /> </a>
+<a href="/pay" id="pay" class="nav btn btn-ghost btn-circle"
+  ><ShoppingCart />
+</a>
