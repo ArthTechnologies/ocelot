09860c545736cf51609cd7c9390e56d575eb575f
DiamonC <88857034+DiamonC@users.noreply.github.com>
2023-08-29 12:37:30 -0500

    fixed incorrect plugin info showing

--- src/lib/components/ui/ChooseVersion.svelte
+++ src/lib/components/ui/ChooseVersion.svelte
@@ -5,15 +5,16 @@
   import { Plus } from "lucide-svelte";
   import PluginResult from "./PluginResult.svelte";
   import { marked } from "marked";
+  import { t } from "$lib/scripts/i18n";
 
   export let id: string;
-  export let pluginName: string;
+  export let name: string;
   export let author: string;
   export let desc: string;
   export let icon: string;
-
   var software = "";
   var sVersion = "";
+
   if (browser) {
     software = localStorage.getItem("serverSoftware");
     sVersion = localStorage.getItem("serverVersion");
@@ -42,6 +43,24 @@
 
       .then((data) => {
         document.getElementById("body").innerHTML = marked(data.body);
+        document.getElementById("pluginTitle").innerHTML = data.title;
+
+        document.getElementById("pluginDesc").innerHTML = data.description;
+        document.getElementById("pluginIcon").src = data.icon_url;
+
+        fetch(lrurl + "team/" + data.team + "/members", {
+          method: "GET",
+
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+          .then((response) => response.json())
+
+          .then((data) => {
+            document.getElementById("pluginAuthor").innerHTML =
+              data[0].user.username;
+          });
       });
 
     let vname = "undefined";
@@ -63,7 +82,7 @@
               type: version.version_type,
               url: version.files[0].url,
               pluginId: id,
-              pluginName: pluginName,
+              pluginName: name,
               modtype: "plugin",
             },
           });
@@ -89,14 +108,47 @@
 <div class="modal">
   <div class="modal-box w-11/12 max-w-5xl space-y-5">
     <div class="pt-6">
-      <PluginResult
-        name={pluginName}
-        {author}
-        {desc}
-        {icon}
-        {id}
-        recursive={true}
-      />
+      <!-- Plugin Result cannot be imported due to a bug where it always says 'Simple Voice Chat'-->
+      <div class="bg-base-200 rounded-lg p-3">
+        <div
+          class="flex justify-between place-items-center max-w-full relative"
+        >
+          <div class="flex space-x-3 flex-shrink-0 w-minus-7">
+            <a href="https://modrinth.com/plugin/{id}" target="_blank">
+              <img
+                id="pluginIcon"
+                src={icon}
+                alt="noicon"
+                class="w-14 h-14 bg-base-300 rounded-lg text-sm"
+              />
+            </a>
+            <div class="max-w-full w-minus-7">
+              <div class="sm:flex gap-1 max-w-full">
+                <a
+                  id="pluginTitle"
+                  href="https://modrinth.com/plugin/{id}"
+                  target="_blank"
+                  class="flex link link-hover text-xl font-bold w-[10rem] md:w-auto break-all sm:break-works"
+                  >{name}</a
+                >
+                <div class="flex space-x-1 place-items-end">
+                  <p>{$t("by")}</p>
+                  <a
+                    id="pluginAuthor"
+                    href="https://modrinth.com/user/{author}"
+                    target="_blank"
+                    class="link link-hover">{author}</a
+                  >
+                </div>
+              </div>
+              <p class="w-minus-7" id="pluginDesc">
+                {desc}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div class="flex justify-between space-x-2 lg:space-x-5 mt-5">
         <div class="">
           <h3 class="font-bold text-2xl mb-4">Description</h3>
