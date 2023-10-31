59dea71aba02337d1c193e5edf19a133a92ed082
DiamonC <88857034+DiamonC@users.noreply.github.com>
2023-10-30 21:37:48 -0500

    when you set the text of a file back, save indicator now goes awy

---
+++ src/lib/components/ui/files/TextEditor.svelte
@@ -0,0 +1,49 @@
+<script>
+  import { browser } from "$app/environment";
+  let origionalText = "";
+
+  function getOrigionalText() {
+    if (browser) {
+      if (document.getElementById("textEditor") != null) {
+        origionalText = document.getElementById("textEditor").value;
+      }
+    }
+  }
+
+  function saveIndicator() {
+    if (document.getElementById("textEditor") != null) {
+      if (document.getElementById("textEditor").value == origionalText) {
+        document.getElementById("filename").innerHTML = document
+          .getElementById("filename")
+          .innerHTML.replace("*", "");
+        document.getElementById("saveButton").classList.add("btn-disabled");
+      } else {
+        //the * indicates it needs to be saved
+        if (!document.getElementById("filename").innerHTML.endsWith("*")) {
+          document.getElementById("filename").innerHTML += "*";
+        }
+
+        //remove btn-disabled from save button
+        document.getElementById("saveButton").classList.remove("btn-disabled");
+      }
+    }
+  }
+
+  //event listener for 'updatedTextEditor' event
+  document.addEventListener("updatedTextEditor", function (e) {
+    getOrigionalText();
+  });
+</script>
+
+<textarea
+  on:input={saveIndicator}
+  class="textarea w-full bg-base-200"
+  id="textEditor"
+/>
+
+<style>
+  textarea {
+    resize: none;
+    height: 90%;
+  }
+</style>
