a2905e7af6688ef923c7cdb530fe9828bc4d6ba9
DiamonC <88857034+DiamonC@users.noreply.github.com>
2023-11-04 11:36:14 -0500

    updated skeleton design

--- src/lib/components/ui/ManageMods.svelte
+++ src/lib/components/ui/ManageMods.svelte
@@ -104,20 +104,38 @@
 
     <div class="space-y-2">
       {#await promise}
-        <div
-          class="flex items-center justify-between bg-base-200 rounded-lg w-full h-[2.75rem] pr-3 py-2 pl-2.5 space-x-1"
-        >
-          <div class="flex items-center space-x-1">
-            <div
-              class="bg-slate-700 rounded-lg w-[17rem] h-[1.5rem] animate-pulse"
-            />
-            <div
-              class="bg-slate-700 rounded-lg w-[1.5rem] h-[1.5rem] animate-pulse"
-            />
+        <div class="flex flex-col">
+          <div
+            class="flex items-center justify-between bg-base-300 rounded-t-lg w-full h-[2.75rem] pr-3 py-2 pl-2.5 space-x-1"
+          >
+            <div class="flex items-center space-x-1">
+              <div
+                class="bg-slate-700 rounded-lg w-[13rem] h-[1.5rem] animate-pulse"
+              />
+              <div
+                class="bg-slate-700 rounded-lg w-[1.5rem] h-[1.5rem] animate-pulse"
+              />
+            </div>
+            <div class="flex space-x-1 items-center">
+              <div
+                class="bg-slate-700 rounded-lg w-[13rem] h-[1.75rem] animate-pulse hidden sm:block"
+              />
+              <button class="btn btn-xs btn-disabled animate-pulse"
+                ><ChevronUp /></button
+              >
+            </div>
           </div>
           <div
-            class="bg-slate-700 rounded-lg w-[13rem] h-[1.75rem] animate-pulse hidden sm:block"
-          />
+            class="h-[4.5rem] bg-base-200 rounded-b-lg px-3 pt-[1.125rem] pb-[.75rem] flex flex-col justify-between"
+          >
+            <div class="flex space-x-1 items-end">
+              <div class="bg-slate-700 rounded-lg w-[5rem] h-4 animate-pulse" />
+              <div class="bg-slate-700 rounded-lg w-[6rem] h-3 animate-pulse" />
+            </div>
+            <div
+              class="bg-slate-700 rounded-lg w-[13rem] h-3.5 animate-pulse"
+            />
+          </div>
         </div>
       {:then}
         {#each res.mods as mod}
