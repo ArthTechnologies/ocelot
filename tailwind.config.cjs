57cc4df2b1640509fc6c3567722eded003593345
DiamonC <88857034+DiamonC@users.noreply.github.com>
2023-11-09 08:10:14 -0600

    updated to daisyui 3.0

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,15 +11,27 @@ require('@tailwindcss/typography')],
         themes: [
             {
                 dark: {
-                    ...require("daisyui/src/colors/themes")[
+                    ...require("daisyui/src/theming/themes")[
                         "[data-theme=dark]"
                     ],
+                    "color-scheme": "dark",
                     primary: "#088385",
+                    "primary-content": "#ffffff",
                     secondary: "#cc4304",
+                    "secondary-content": "#ffffff",
+                    accent: "#1FB2A5",
+                    "accent-content": "#ffffff",
+                    neutral: "#191D24",
+                    "neutral-focus": "#111318",
+                    "neutral-content": "#A6ADBB",
+                    "base-100": "#2A303C",
+                    "base-200": "#242933",
+                    "base-300": "#20252E",
+                    "base-content": "#A6ADBB",
 
                 },
                 light: {
-                    ...require("daisyui/src/colors/themes")[
+                    ...require("daisyui/src/theming/themes")[
                         "[data-theme=light]"
                     ],
                     primary: "#cc4304",
