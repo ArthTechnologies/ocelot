35e18dbde8e727aa2b2216aaa5319f77e0ca9a96
DiamonC <88857034+DiamonC@users.noreply.github.com>
2023-11-12 21:01:18 -0600

    fixed helper going under worldgen mods

--- src/lib/components/ui/Helper.svelte
+++ src/lib/components/ui/Helper.svelte
@@ -4,7 +4,7 @@
   export let tooltipText: string;
 </script>
 
-<div class="tooltip tooltip-right z-40 hidden sm:block" data-tip={tooltipText}>
+<div class="tooltip tooltip-right z-50 hidden sm:block" data-tip={tooltipText}>
   <div class="btn btn-circle btn-ghost btn-md">
     <HelpCircle />
   </div>
