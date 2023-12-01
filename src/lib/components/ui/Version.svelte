36290c62cbf9a38704e51028d4f20f8b2aff9fb0
DiamonC <88857034+DiamonC@users.noreply.github.com>
2023-11-30 21:39:18 -0600

    fixed icons shrinking

--- src/lib/components/ui/Version.svelte
+++ src/lib/components/ui/Version.svelte
@@ -84,13 +84,13 @@
             class="bg-base-300 flex px-2 py-1 rounded-md place-items-center text-sm w-[13rem]"
           >
             {#if dependency.dependency_type == "optional"}
-              <InfoIcon class="mr-1.5" size="16" />
+              <InfoIcon class="mr-1.5 shrink-0" size="16" />
               {$t("worksWith")}
             {:else if dependency.dependency_type == "incompatible"}
-              <AlertCircle class="mr-1.5" size="16" />
+              <AlertCircle class="mr-1.5 shrink-0" size="16" />
               {$t("incompatibleWith")}
             {:else}
-              <AlertCircle class="mr-1.5" size="16" />
+              <AlertCircle class="mr-1.5 shrink-0" size="16" />
               {$t("requires")}
             {/if}
 
