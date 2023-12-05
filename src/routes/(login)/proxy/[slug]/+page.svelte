f99c33969c2a2c16734b2c680f05d3829a5fe653
DiamonC <88857034+DiamonC@users.noreply.github.com>
2023-12-04 20:01:39 -0600

    fixed various visual bugs with proxy terminal

--- src/routes/(login)/proxy/[slug]/+page.svelte
+++ src/routes/(login)/proxy/[slug]/+page.svelte
@@ -499,11 +499,11 @@
     class="space-x-7 xs:flex xs:flex-col-reverse md:flex justify-between p-10"
   >
     <div class="flex flex-col items-center space-y-3 md:space-y-0">
-      <div id="terminalContainerContainer" class="relative">
+      <div id="terminalContainerContainer" class="relative mb-1.5">
         <FullscreenTerminal />
         <div
           id="terminalContainer"
-          class="bg-base-300 h-96 rounded-xl shadow-xl overflow-auto w-[20rem] lg:w-[30rem] xl:w-[50rem] 2xl:w-[60rem]"
+          class="bg-base-300 h-96 rounded-xl overflow-auto w-[20rem] lg:w-[30rem] xl:w-[50rem] 2xl:w-[60rem]"
         >
           <div class="p-5 sm:text-xs xl:text-base font-mono relative">
             <p id="terminal" />
