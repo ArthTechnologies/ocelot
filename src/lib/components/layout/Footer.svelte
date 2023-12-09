f3350354c66d066d2f6a8dc332340f07a78274d7
DiamonC <88857034+DiamonC@users.noreply.github.com>
2023-12-09 13:45:23 -0600

    made visual tweaks to the footer

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -2,7 +2,7 @@
   import { browser } from "$app/environment";
 
   import { t, locale, locales } from "$lib/scripts/i18n";
-  import { Mail } from "lucide-svelte";
+  import { HelpCircle, Mail } from "lucide-svelte";
   import LanguageSwitcher from "../buttons/LanguageSwitcher.svelte";
   type NavType = "default" | "welcome";
 
@@ -18,21 +18,8 @@
     style="margin:0rem;"
     class="btn btn-sm btn-ghost flex items-center justify-self-end"
   >
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="18"
-      height="18"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      class="feather feather-help-circle mt-[0.05rem] mr-1.5"
-      ><circle cx="12" cy="12" r="10" /><path
-        d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"
-      /><line x1="12" y1="17" x2="12.01" y2="17" /></svg
-    >{$t("help")}
+    <HelpCircle class="mr-1.5 mt-[0.05rem]" size="18" />
+    {$t("help")}
   </label>
 </footer>
 
