bb7d2a3b07c93e1226e9963dc7a59fc9675bbc3a
DiamonC <88857034+DiamonC@users.noreply.github.com>
2023-12-16 14:50:12 -0600

    added less-buggy diasyui theme switcher for supported browsers

--- src/lib/components/buttons/ThemeToggle.svelte
+++ src/lib/components/buttons/ThemeToggle.svelte
@@ -2,10 +2,19 @@
   import { browser } from "$app/environment";
   import darkMode from "$lib/stores/darkMode";
   import { Moon, Sun } from "lucide-svelte";
-
+  let supportsDaisyUIThemeSwitcher = false;
   if (browser) {
-    if (localStorage.getItem("theme") == "light") {
-    }
+    console.log(navigator.userAgent);
+    // check if the browser is chrome 105+, firefox 121+, or safari 15.4+
+    supportsDaisyUIThemeSwitcher =
+      (navigator.userAgent.includes("Chrome") &&
+        parseInt(navigator.userAgent.match(/Chrome\/(\d+)\./)[1]) >= 105) ||
+      (navigator.userAgent.includes("Firefox") &&
+        parseInt(navigator.userAgent.match(/Firefox\/(\d+)\./)[1]) >= 121) ||
+      (navigator.userAgent.includes("Safari") &&
+        parseInt(navigator.userAgent.match(/Version\/(\d+)\./)[1]) >= 15);
+
+    //to-do: find out what versions of edge and maybe opera supports this
   }
 
   function refreshTheme() {
@@ -18,17 +27,33 @@
   }
 </script>
 
-<label class=" btn btn-circle btn-ghost swap swap-rotate">
-  <!-- this hidden checkbox controls the state -->
-  <input
-    data-toggle-theme="light,dark"
-    data-act-class="ACTIVECLASS"
-    type="checkbox"
-    bind:value={$darkMode}
-    on:click={refreshTheme}
-  />
+{#if supportsDaisyUIThemeSwitcher}
+  <label class=" btn btn-circle btn-ghost swap swap-rotate">
+    <!-- this hidden checkbox controls the state -->
+    <input
+      class="theme-controller"
+      value="light"
+      type="checkbox"
+      on:click={refreshTheme}
+    />
 
-  <Sun class="swap-on" />
+    <Sun class="swap-on" />
 
-  <Moon class="swap-off" />
-</label>
+    <Moon class="swap-off" />
+  </label>
+{:else}
+  <label class=" btn btn-circle btn-ghost swap swap-rotate">
+    <!-- this hidden checkbox controls the state -->
+    <input
+      data-toggle-theme="light,dark"
+      data-act-class="ACTIVECLASS"
+      type="checkbox"
+      bind:value={$darkMode}
+      on:click={refreshTheme}
+    />
+
+    <Sun class="swap-on" />
+
+    <Moon class="swap-off" />
+  </label>
+{/if}
