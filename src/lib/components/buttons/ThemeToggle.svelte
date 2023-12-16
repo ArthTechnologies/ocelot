5229b6cde76f064517118eb374789eb9fafb53a1
DiamonC <88857034+DiamonC@users.noreply.github.com>
2023-12-16 15:01:10 -0600

    theme switcher doesnt show if broswer doesnt support it

--- src/lib/components/buttons/ThemeToggle.svelte
+++ src/lib/components/buttons/ThemeToggle.svelte
@@ -3,9 +3,10 @@
   import darkMode from "$lib/stores/darkMode";
   import { Moon, Sun } from "lucide-svelte";
   let supportsDaisyUIThemeSwitcher = false;
+  let supportsOklch = true;
   if (browser) {
     console.log(navigator.userAgent);
-    // check if the browser is chrome 105+, firefox 121+, or safari 15.4+
+    // checks if the browser (supports DaisyUI theme switcher) is chrome 105+, firefox 121+, or safari 15.4+
     supportsDaisyUIThemeSwitcher =
       (navigator.userAgent.includes("Chrome") &&
         parseInt(navigator.userAgent.match(/Chrome\/(\d+)\./)[1]) >= 105) ||
@@ -15,6 +16,22 @@
         parseInt(navigator.userAgent.match(/Version\/(\d+)\./)[1]) >= 15);
 
     //to-do: find out what versions of edge and maybe opera supports this
+
+    // checks if the browser (supports oklch and thus custom themes) isn't chrome 110-, firefox 112-, edge 110-, or opera 96-
+    try {
+      supportsOklch = !(
+        (navigator.userAgent.includes("Chrome") &&
+          parseInt(navigator.userAgent.match(/Chrome\/(\d+)\./)[1]) <= 110) ||
+        (navigator.userAgent.includes("Firefox") &&
+          parseInt(navigator.userAgent.match(/Firefox\/(\d+)\./)[1]) <= 112) ||
+        (navigator.userAgent.includes("Edge") &&
+          parseInt(navigator.userAgent.match(/Edge\/(\d+)\./)[1]) <= 110) ||
+        (navigator.userAgent.includes("Opera") &&
+          parseInt(navigator.userAgent.match(/Opera\/(\d+)\./)[1]) <= 96)
+      );
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   function refreshTheme() {
@@ -27,33 +44,35 @@
   }
 </script>
 
-{#if supportsDaisyUIThemeSwitcher}
-  <label class=" btn btn-circle btn-ghost swap swap-rotate">
-    <!-- this hidden checkbox controls the state -->
-    <input
-      class="theme-controller"
-      value="light"
-      type="checkbox"
-      on:click={refreshTheme}
-    />
+{#if supportsOklch}
+  {#if supportsDaisyUIThemeSwitcher}
+    <label class=" btn btn-circle btn-ghost swap swap-rotate">
+      <!-- this hidden checkbox controls the state -->
+      <input
+        class="theme-controller"
+        value="light"
+        type="checkbox"
+        on:click={refreshTheme}
+      />
 
-    <Sun class="swap-on" />
+      <Sun class="swap-on" />
 
-    <Moon class="swap-off" />
-  </label>
-{:else}
-  <label class=" btn btn-circle btn-ghost swap swap-rotate">
-    <!-- this hidden checkbox controls the state -->
-    <input
-      data-toggle-theme="light,dark"
-      data-act-class="ACTIVECLASS"
-      type="checkbox"
-      bind:value={$darkMode}
-      on:click={refreshTheme}
-    />
+      <Moon class="swap-off" />
+    </label>
+  {:else}
+    <label class=" btn btn-circle btn-ghost swap swap-rotate">
+      <!-- this hidden checkbox controls the state -->
+      <input
+        data-toggle-theme="light,dark"
+        data-act-class="ACTIVECLASS"
+        type="checkbox"
+        bind:value={$darkMode}
+        on:click={refreshTheme}
+      />
 
-    <Sun class="swap-on" />
+      <Sun class="swap-on" />
 
-    <Moon class="swap-off" />
-  </label>
+      <Moon class="swap-off" />
+    </label>
+  {/if}
 {/if}
