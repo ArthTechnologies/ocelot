166e9946228b2a94673e289b12aa1a7295be9be0
DiamonC <diamonc@proton.me>
2023-12-19 19:30:48 -0600

    fixed files route not working with languages

--- routes/file.js
+++ routes/file.js
@@ -2,28 +2,27 @@ const express = require("express");
 const Router = express.Router();
 const fs = require("fs");
 
-
 Router.get("/:posttype/:filename", (req, res) => {
-	  let path = "files/" + req.params.posttype + "/" + req.params.filename;
-	let text = "";
-	//log the current working directory
-	console.log("path: " + path);
-	//log the path
+  let path =
+    "files/" +
+    req.params.posttype +
+    "/" +
+    req.params.filename.split("*").join("/");
+  let text = "";
+  //log the current working directory
+  console.log("path: " + path);
+  //log the path
 
-	if (fs.existsSync(path)) {
-
-		fs.readFile(path, "utf8", (err, data) => {
-			if (err) {
-				console.log(err);
-			}
-			res.send(data)
-		});
-
-	} else {
-
-		res.send({"msg":"File not found"})
-	}
-
+  if (fs.existsSync(path)) {
+    fs.readFile(path, "utf8", (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      res.send(data);
+    });
+  } else {
+    res.send({ msg: "File not found" });
+  }
 });
 
 module.exports = Router;
