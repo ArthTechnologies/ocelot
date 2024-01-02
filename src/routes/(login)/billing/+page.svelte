c69bfabd5e144e5f9757dac8053ff37e2da771d2
DiamonC <88857034+DiamonC@users.noreply.github.com>
2024-01-02 16:33:19 -0600

    modified billing to show both subscriptions and servers

--- src/routes/(login)/billing/+page.svelte
+++ src/routes/(login)/billing/+page.svelte
@@ -1,25 +1,42 @@
 <script lang="ts">
-  import { customerPortalLink } from "$lib/scripts/req";
+  import { apiurl, customerPortalLink } from "$lib/scripts/req";
   import { t, locale, locales } from "$lib/scripts/i18n";
   import { browser } from "$app/environment";
 
-  let servers = 0;
-  if (browser) {
-    servers = localStorage.getItem("amountOfServers");
-  }
+  let servers = [];
   var email: string = "";
-  //get subs and servers from localstorage
-  var subs = 0;
+  let promise = null;
+  //gets subs and servers from localstorage
+  var subs = {
+    subscriptions: 0,
+    basicSubscriptions: 0,
+    moddedSubscriptions: 0,
+  };
+  let address;
   if (browser) {
-    subs = localStorage.getItem("subs");
-  }
-  //set email to the value of localstorage.getItem("email") if window is defined
-  if (browser) {
+    servers = JSON.parse(localStorage.getItem("servers"));
+    address = localStorage.getItem("address");
+
+    promise = fetch(apiurl + "servers/purchases", {
+      method: "GET",
+      headers: {
+        username: localStorage.getItem("accountEmail"),
+        token: localStorage.getItem("token"),
+      },
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        subs = json;
+      });
+
+    //sets email to the value of localstorage.getItem("email")
+
     email = localStorage.getItem("accountEmail");
     console.log(email);
   }
   function subscribe() {
-    //set localstorage item "subscribed" to true if localstorage is defined
+    //sets localstorage item "subscribed" to true
     if (browser) {
       localStorage.setItem("subscribed", "true");
     }
@@ -32,9 +49,58 @@
 <div class="flex place-content-center">
   <div class="flex flex-col grow items-center max-w-[55rem] mb-10 space-y-6">
     <h1 class="divider px-10 text-3xl font-semibold">{$t("bill.title")}</h1>
-    <div class="stat bg-base-200 text-center rounded-xl w-3/4 shadow">
-      <div class="stat-title">{$t("navbar.servers")}</div>
-      <div class="stat-value">{servers}</div>
+    <div class="px-6 py-4 bg-base-200 rounded-xl w-3/4 shadow">
+      <div>{$t("subscriptions")}</div>
+      <div class="flex gap-1">
+        {#await promise}
+          <div
+            class="bg-slate-700 animate-pulse
+           w-14 h-5 rounded-md"
+          ></div>
+        {:then}
+          {#if subs.subscriptions > 0 && subs.basicSubscriptions == 0 && subs.moddedSubscriptions == 0}
+            {#each Array(subs.subscriptions) as server}
+              <div
+                class="bg-gradient-to-tr from-orange-500 to-pink-600 w-min px-1.5 rounded-md text-sm text-black font-bold"
+              >
+                {subs.subscriptions.length}
+              </div>
+            {/each}
+          {/if}
+          {#each Array(subs.basicSubscriptions) as server}
+            <div
+              class="bg-gradient-to-tr from-orange-500 to-pink-600 w-min px-1.5 rounded-md text-sm text-black"
+            >
+              {$t("basic")}
+            </div>
+          {/each}
+
+          {#each Array(subs.moddedSubscriptions) as server}
+            <div
+              class="bg-gradient-to-tr from-cyan-500 to-indigo-600 w-min px-1.5 rounded-md text-sm text-black"
+            >
+              {$t("modded")}
+            </div>
+          {/each}
+        {/await}
+      </div>
+
+      <div class="mt-1">{$t("navbar.servers")}</div>
+      <div class="flex gap-1">
+        {#each servers as server}
+          {#if JSON.stringify(server).includes(":not created yet")}
+            <div
+              class="bg-base-300 w-min px-1.5 rounded-md text-sm text-gray-500"
+            >
+              {address}:{10000 + parseInt(server.id)}
+            </div>
+          {:else}
+            <div class="bg-base-300 w-min px-1.5 rounded-md text-sm">
+              {address}:{10000 + parseInt(server.id)}
+            </div>
+          {/if}
+        {/each}
+      </div>
     </div>
     <div
       class="flex flex-wrap justify-center button-container sm:space-x-3 w-[90%]"
