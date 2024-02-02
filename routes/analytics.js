a6c65815c66b96d4c6ddd18ef579084df6b81067
DiamonC <diamonc@proton.me>
2024-02-02 16:48:11 -0600

    debug log, fixed mac being 'unknown' type

--- routes/analytics.js
+++ routes/analytics.js
@@ -1,56 +1,49 @@
-
 const express = require("express");
 const Router = express.Router();
 const fs = require("fs");
 
 Router.get("/", (req, res) => {
-    let analytics = JSON.parse(fs.readFileSync("analytics.json"));
-    res.send(analytics);
+  let analytics = JSON.parse(fs.readFileSync("analytics.json"));
+  res.send(analytics);
 });
 
 //disabled until new privacy policy goes into effect
 Router.post("/", (req, res) => {
+  //how many days since 1970
+  let day = new Date().getTime() / 1000 / 60 / 60 / 24;
+  day = parseInt(day.toString().split(".")[0]);
 
-        //how many days since 1970
-    let day = new Date().getTime() / 1000 / 60 / 60 / 24;
-    day = parseInt(day.toString().split('.')[0]);
-    
-    let platform = req.body.platform;
-    let analytics = JSON.parse(fs.readFileSync("analytics.json"));
-    analytics.day = day;
-    analytics.hits++;
-    if (analytics.days[day] == undefined){
-        analytics.days[day] = 1;
-    } else {
-        analytics.days[day]++;
-        if (analytics.days[day] > analytics.max){
-            analytics.max = analytics.days[day];
-        }
+  let platform = req.body.platform;
+  let analytics = JSON.parse(fs.readFileSync("analytics.json"));
+  analytics.day = day;
+  analytics.hits++;
+  if (analytics.days[day] == undefined) {
+    analytics.days[day] = 1;
+  } else {
+    analytics.days[day]++;
+    if (analytics.days[day] > analytics.max) {
+      analytics.max = analytics.days[day];
     }
+  }
 
-    if(platform.includes("Linux")){
+  if (platform.includes("Linux")) {
+    analytics.devices.linux++;
+  } else if (platform.includes("Win")) {
+    analytics.devices.windows++;
+  } else if (platform.includes("Mac")) {
+    analytics.devices.macintosh++;
+  } else if (platform.includes("Android")) {
+    analytics.devices.android++;
+  } else if (platform.includes("iP")) {
+    analytics.devices.iOS++;
+  } else {
+    analytics.devices.unknown++;
+  }
 
-        analytics.devices.linux++;
-    } else if (platform.includes("Win")){
-
-        analytics.devices.windows++;
-    } else if (platform.includes("Macintosh")){
-
-        analytics.devices.macintosh++;
-    } else if (platform.includes("Android")){
-
-        analytics.devices.android++;
-    } else if (platform.includes("iP")){
-
-        analytics.devices.iOS++;
-    } else {
-        analytics.devices.unknown++;
-    }
-
-    console.log("platform: " + platform)
-    fs.writeFileSync("analytics.json", JSON.stringify(analytics));
-    res.send({"msg":"ok"})
-
+  console.log("platform: " + platform);
+  console.log("userAgent: " + req.body.userAgent);
+  fs.writeFileSync("analytics.json", JSON.stringify(analytics));
+  res.send({ msg: "ok" });
 });
 
 module.exports = Router;
