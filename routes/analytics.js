98b8598a1479fb5edc7662a644e4570035e29644
DiamonC <diamonc@proton.me>
2024-06-24 14:11:56 -0500

    removed debug log

--- routes/analytics.js
+++ routes/analytics.js
@@ -9,7 +9,6 @@ Router.get("/", (req, res) => {
 
 //disabled until new privacy policy goes into effect
 Router.post("/", (req, res) => {
-  console.log(req.body);
   let userAgent = req.body.userAgent;
   //this makes sure google crawlers arent counted in analytics
   if (!userAgent.includes("google.com/") && !userAgent.includes("bot")) {
