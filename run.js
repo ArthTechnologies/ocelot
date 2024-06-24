f1fb4eb9a5592769b93c6793c6677a77a5fdb57d
DiamonC <diamonc@proton.me>
2024-06-24 13:40:54 -0500

    .

--- run.js
+++ run.js
@@ -35,12 +35,12 @@ if (!fs.existsSync("analytics.json")) {
 
 if (!fs.existsSync("files/backups")) {
   fs.mkdirSync("files/backups");
-  if (JSON.parse(fs.readFileSync("analytics.json"))) {
-    fs.writeFileSync(
-      "files/backups/analytics.json",
-      fs.readFileSync("analytics.json")
-    );
-  }
+}
+if (JSON.parse(fs.readFileSync("analytics.json"))) {
+  fs.writeFileSync(
+    "files/backups/analytics.json",
+    fs.readFileSync("analytics.json")
+  );
 }
 // middlewares
 app.use(express.json(), cors());
