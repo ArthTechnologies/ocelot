3c506bfac25c2eb56a854ce80a152e34ba9e5be5
DiamonC <88857034+DiamonC@users.noreply.github.com>
2024-07-17 15:37:44 -0500

    added frontend support for basic customer data in dashboard

--- src/routes/(login)/dashboard/+page.svelte
+++ src/routes/(login)/dashboard/+page.svelte
@@ -1,12 +1,19 @@
 <script>
   import { browser } from "$app/environment";
   import Analytics from "$lib/components/pages/dashboard/Analytics.svelte";
+  import { t } from "$lib/scripts/i18n";
   import { apiurl } from "$lib/scripts/req";
   import { alert } from "$lib/scripts/utils";
+  import { FishOff } from "lucide-svelte";
   import { fade } from "svelte/transition";
 
   let isLoggedIn = false;
-
+  let address = "";
+  let customers = [];
+  if (browser) {
+    address = localStorage.getItem("address");
+  }
+  let loaded = false;
   function login() {
     if (browser) {
       let input = document.getElementById("input")?.value;
@@ -18,6 +25,15 @@
         .then((data) => {
           if (data.success) {
             isLoggedIn = true;
+
+            fetch(apiurl + "dashboard/customers?tempToken=" + input, {
+              method: "GET",
+            })
+              .then((res) => res.json())
+              .then((data) => {
+                customers = data;
+                loaded = true;
+              });
           } else {
             alert("Expired or invalid token");
           }
@@ -29,7 +45,7 @@
 {#if !isLoggedIn}
   <div
     transition:fade={{ duration: 1200 }}
-    class="absolute h-[200rem] w-screen bg-base-100 z-40 backdrop-blur-sm bg-opacity-70"
+    class="absolute h-[200rem] w-screen bg-base-100 z-40 backdrop-blur-sm bg-opacity-70 -mt-6"
   ></div>
   <div class="absolute mt-4 w-96 z-50" transition:fade={{ duration: 600 }}>
     <div class="bg-base-100 w-96 shadow-xl">
@@ -67,5 +83,47 @@
   </div>
 {/if}
 <div class="flex gap-5 justify-end px-24 -mt-4">
+  {#if !loaded}
+    <div class="flex flex-col gap-5 w-96">
+      {#each Array.from({ length: 10 }) as _}
+        <div class="px-6 py-4 bg-base-200 rounded-xl w-3/4 shadow space-y-1.5">
+          <div class="bg-slate-700 animate-pulse w-20 h-5 rounded-md"></div>
+          <div class="bg-slate-700 animate-pulse w-14 h-5 rounded-md"></div>
+          <div class="bg-slate-700 animate-pulse w-14 h-5 rounded-md"></div>
+        </div>
+      {/each}
+    </div>
+  {:else}
+    <div class="flex flex-col gap-5 w-96">
+      {#each customers as customer}
+        <div class="px-6 py-4 bg-base-200 rounded-xl w-3/4 shadow space-y-1.5">
+          {customer[0].email}
+          <div class="flex gap-1">
+            {#if customer[0].activeBasicSubscriptions > 0}
+              <div
+                class="bg-gradient-to-tr from-orange-400 to-pink-500 px-1.5 rounded-md text-sm text-black"
+              >
+                {customer[0].activeBasicSubscriptions}x {$t("basic")}
+              </div>
+            {/if}
+            {#if customer[0].activeModdedSubscriptions > 0}
+              <div
+                class="bg-gradient-to-tr from-cyan-400 to-indigo-500 px-1.5 rounded-md text-sm text-black"
+              >
+                {customer[0].activeModdedSubscriptions}x {$t("modded")}
+              </div>
+            {/if}
+          </div>
+          <div class="flex gap-1">
+            {#each customer[1].servers as server}
+              <div class="bg-neutral px-1.5 rounded-md text-sm">
+                {address}:{server}
+              </div>
+            {/each}
+          </div>
+        </div>
+      {/each}
+    </div>
+  {/if}
   <Analytics />
 </div>
