854b5a2835fff36eace49f255cb785955de5f0db
DiamonC <diamonc@proton.me>
2024-07-29 17:49:22 -0500

    added site to status

--- routes/status.js
+++ routes/status.js
@@ -2,66 +2,72 @@ const express = require("express");
 const Router = express.Router();
 const fs = require("fs");
 
-
 Router.get("/", (req, res) => {
-	let resp = {};
-	
-	let request = require("request");
-	request("https://us-dallas-1.arthmc.xyz", function(error, response, body){
-		if (error){
-			resp["quartz"] = "Offline";
-		} else {
-			if (response.statusCode != 502) {
-			resp["quartz"] = "Online";
-			} else {
-				resp["quartz"] = "Offline";
-			}
-		}
-	});
+  let resp = {};
 
-	request("https://servers.arthmc.xyz", function(error, response, body){
-		if (error){
-			resp["observer"] = "Offline";
-		} else {
-			if (response.statusCode != 502) {
-			resp["observer"] = "Online";
-			} else {
-				resp["observer"] = "Offline";
-			}
-		}
-	});
+  let request = require("request");
+  request("https://us-dallas-1.arthmc.xyz", function (error, response, body) {
+    if (error) {
+      resp["quartz"] = "Offline";
+    } else {
+      if (response.statusCode != 502) {
+        resp["quartz"] = "Online";
+      } else {
+        resp["quartz"] = "Offline";
+      }
+    }
+  });
 
+  request("https://servers.arthmc.xyz", function (error, response, body) {
+    if (error) {
+      resp["observer"] = "Offline";
+    } else {
+      if (response.statusCode != 502) {
+        resp["observer"] = "Online";
+      } else {
+        resp["observer"] = "Offline";
+      }
+    }
+  });
+  request("https://arthmc.xyz", function (error, response, body) {
+    if (error) {
+      resp["frontend"] = "Offline";
+    } else {
+      if (response.statusCode != 502) {
+        resp["frontend"] = "Online";
+      } else {
+        resp["frontend"] = "Offline";
+      }
+    }
+  });
 
-	
+  let net = require("net");
+  let client = new net.Socket();
+  client.connect(25565, "arthmc.xyz", function () {
+    resp["arthnetwork"] = "Online";
+    client.destroy();
+  });
+  client.on("error", function (err) {
+    console.log(err);
+    resp["arthnetwork"] = "Offline";
+  });
 
-	let net = require("net");
-	let client = new net.Socket();
-	client.connect(25565, "arthmc.xyz", function() {
-		resp["arthnetwork"] = "Online";
-		client.destroy();
-	});
-	client.on('error', function(err) {
-		console.log(err);
-		resp["arthnetwork"] = "Offline";
-	});
-
-	let inter = 10;
-	//check every 50 ms
-	let interval = setInterval(function(){
-		if(resp["arthnetwork"] != undefined && resp["observer"] != undefined && resp["quartz"] != undefined){
-			clearInterval(interval);
-			res.send(resp);
-		}
-		inter++;
-		if (inter > 50){
-			inter = 50;
-		}
-	}, inter);
-
-
-
-
-
+  let inter = 10;
+  //check every 50 ms
+  let interval = setInterval(function () {
+    if (
+      resp["arthnetwork"] != undefined &&
+      resp["observer"] != undefined &&
+      resp["quartz"] != undefined
+    ) {
+      clearInterval(interval);
+      res.send(resp);
+    }
+    inter++;
+    if (inter > 50) {
+      inter = 50;
+    }
+  }, inter);
 });
 
 module.exports = Router;
