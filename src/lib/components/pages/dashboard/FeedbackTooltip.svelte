1dc6d65f9d651bbf2255386bcaf8f0690bcf5d6c
DiamonC <88857034+DiamonC@users.noreply.github.com>
2024-07-29 13:41:36 -0500

    ui improvements for customer feedback

---
+++ src/lib/components/pages/dashboard/FeedbackTooltip.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { Info, Mail } from "lucide-svelte";
+
+  export let feedback: string;
+</script>
+
+{#if feedback != "null" && feedback != undefined}
+  {#if feedback == "unused" || feedback.includes("_")}
+    <div
+      class="tooltip tooltip-right z-50 hidden sm:block"
+      data-tip="Feedback: {feedback}"
+    >
+      <Info size="16" class="mt-0.5" />
+    </div>
+  {:else}
+    <div
+      class="tooltip tooltip-right z-50 hidden sm:block"
+      data-tip="Feedback: {feedback}"
+    >
+      <Mail size="16" class="mt-0.5" />
+    </div>
+  {/if}
+{/if}
