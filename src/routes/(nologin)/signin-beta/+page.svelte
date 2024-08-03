577f14e6695f9b956684efaca934b6f2331b72ce
DiamonC <88857034+DiamonC@users.noreply.github.com>
2024-08-03 17:17:17 -0500

    fixed email not being included in discord response

--- src/routes/(nologin)/signin-beta/+page.svelte
+++ src/routes/(nologin)/signin-beta/+page.svelte
@@ -34,7 +34,7 @@
       <div class="flex gap-2 items-center">
         <a
           class="btn btn-neutral mb-2 btn-icon-text text-2xs"
-          href="https://discord.com/api/oauth2/authorize?client_id=1025856388297150475&redirect_uri={address}/auth/discord&response_type=token&scope=identify"
+          href="https://discord.com/api/oauth2/authorize?client_id=1025856388297150475&redirect_uri={address}/auth/discord&response_type=token&scope=email+identify"
           ><img
             alt="microsoft logo"
             style="width:2.5ch"
