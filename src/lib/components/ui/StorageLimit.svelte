e65a164c12fcf5a0a753d2bf54d416b0cdd2ae49
DiamonC <88857034+DiamonC@users.noreply.github.com>
2024-08-05 22:02:38 -0500

    changed storage indicator so that it shows what is taking up storage instead of just a gradient

--- src/lib/components/ui/StorageLimit.svelte
+++ src/lib/components/ui/StorageLimit.svelte
@@ -8,14 +8,20 @@
   let storageRatio = "0/0mB";
   let theme = "dark";
   let res = {};
+  let modsplugins = 0;
+  let limit = 0;
+  let first = 0;
+  let second = 0;
+  let third = 0;
+  let firstColor = "bg-neutral";
+  let secondColor = "bg-slate-500";
+  let thirdColor = "bg-slate-700";
+  let firstName = "Mods/Plugins";
+  let secondName = "Worlds";
+  let thirdName = "Misc";
+  let misc = 0;
   onMount(() => {
     if (browser) {
-      //this listens for whenever the theme is changed
-      window.addEventListener("refreshTheme", () => {
-        setTimeout(() => {
-          setGradient();
-        });
-      });
       let baseurl = apiurl;
       if (usingOcelot)
         baseurl =
@@ -39,43 +45,65 @@
             storageRatio = fileSizeShort(data.used);
           } else {
             storageRatio = downloadProgressShort(data.used, data.limit);
+            modsplugins = data.plugins + data.mods;
+            limit = data.limit;
+            misc = data.used - modsplugins - data.worlds;
 
-            setGradient();
+            first = Math.max(modsplugins, data.worlds, misc);
+            second = Math.min(modsplugins, data.worlds, misc);
+            third = modsplugins + data.worlds + misc - first - second;
+
+            if (first == modsplugins) {
+              firstName = "Mods/Plugins";
+            } else if (first == data.worlds) {
+              firstName = "World";
+            } else {
+              firstName = "Misc";
+            }
+
+            if (second == modsplugins) {
+              secondName = "Mods/Plugins";
+            } else if (second == data.worlds) {
+              secondName = "World";
+            } else {
+              secondName = "Misc";
+            }
+
+            if (third == modsplugins) {
+              thirdName = "Mods/Plugins";
+            } else if (third == data.worlds) {
+              thirdName = "World";
+            } else {
+              thirdName = "Misc";
+            }
           }
         });
     }
   });
-
-  function setGradient() {
-    if (browser) {
-      const ratioVisualizer = document.getElementById("ratioVisualizer");
-      theme = localStorage.getItem("theme");
-      console.log("refreshing theme" + theme);
-      if (theme == "dark") {
-        ratioVisualizer.style.background = `linear-gradient(
-  to right,
-  rgba(0, 0, 0, 0.9) 0%,
-  rgba(0, 0, 0, 0.0) ${(res.used / res.limit) * 100}%,
-  #088587 ${(res.used / res.limit) * 100}%,
-  #088587 100%
-)`;
-      } else if (theme == "light") {
-        ratioVisualizer.style.background = `linear-gradient(
-  to right,
-  rgba(0, 0, 0, 0.9) 0%,
-  rgba(0, 0, 0, 0.7) ${(res.used / res.limit) * 100}%,
-  #088587 ${(res.used / res.limit) * 100}%,
-  #088587 100%)`;
-      }
-    }
-  }
 </script>
 
 <div
   id="ratioVisualizer"
-  class="bg-base-200 border-base-300 border-2 rounded-lg px-4 flex items-center w-44 font-semibold text-sm uppercase text-white gap-1.5"
+  class="relative bg-[#242933] border-base-300 border-2 rounded-lg px-4 flex items-center w-44 font-semibold text-sm uppercase text-white gap-1.5"
 >
-  <HardDrive />
-  {$t("using")}
-  {storageRatio}
+  <HardDrive class="z-[5]" />
+  <span class="z-[5]">{$t("using")} {storageRatio}</span>
+
+  <div
+    class="absolute left-0 h-full {firstColor} z-[3] rounded-l-[0.4rem] tooltip"
+    style="width: {(first / limit) * 100}%"
+    data-tip={firstName}
+  ></div>
+  <div
+    class="absolute left-0 h-full {secondColor} z-[2] rounded-l-xl tooltip"
+    style="width: {(second / limit) * 100 + (first / limit) * 100}%;"
+    data-tip={secondName}
+  ></div>
+  <div
+    class="absolute left-0 h-full {thirdColor} z-[1] rounded-l-xl tooltip"
+    style="width: {(third / limit) * 100 +
+      (second / limit) * 100 +
+      (first / limit) * 100}%"
+    data-tip={thirdName}
+  ></div>
 </div>
