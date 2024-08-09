00aa0d92fac2ae23fe450b6a8084eb2ba2d9ce03
DiamonC <88857034+DiamonC@users.noreply.github.com>
2024-08-09 11:51:21 -0500

    made navbar better on mobile

--- src/lib/components/buttons/Home.svelte
+++ src/lib/components/buttons/Home.svelte
@@ -2,6 +2,6 @@
   import { Home } from "lucide-svelte";
 </script>
 
-<div class="visible md:hidden nav" id="servers2">
+<div class="visible max-[450px]:hidden md:hidden nav" id="servers2">
   <a href="/" class="btn btn-ghost btn-circle"><Home /> </a>
 </div>
