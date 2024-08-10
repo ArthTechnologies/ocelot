b05266ed9b42b07b55c5160d2e55eb6e8cfbe7a9
DiamonC <diamonc@proton.me>
2024-08-10 13:57:33 -0500

    fixed issues with json

--- scripts/utils.js
+++ scripts/utils.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 function readJSON(file) {
   let json = {};
   try {
-    json = JSON.parse(fs.readFileSync(file, "utf8"));
+    json = JSON.parse(fs.readFileSync(file, "utf8").trim());
   } catch (error) {
     console.log("error parsing json for " + file, error);
   }
@@ -11,7 +11,7 @@ function readJSON(file) {
 
 function writeJSON(file, json) {
   try {
-    fs.writeFileSync(file, JSON.stringify(json, null, 2));
+    fs.writeFileSync(file, json, "utf8");
   } catch (error) {
     console.log("error writing json for " + file, error);
   }
