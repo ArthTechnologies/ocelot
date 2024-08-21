4ce9d83c8650f7f822b70c05b0e6da2cdb52264a
DiamonC <diamonc@proton.me>
2024-08-20 20:43:05 -0500

    debug log

--- routes/analytics.js
+++ routes/analytics.js
@@ -13,6 +13,7 @@ Router.post("/", (req, res) => {
   let userAgent = req.body.userAgent;
   //this makes sure google crawlers arent counted in analytics
   if (!userAgent.includes("google.com/") && !userAgent.includes("bot")) {
+    console.log("test");
     //how many days since 1970
     let day = new Date().getTime() / 1000 / 60 / 60 / 24;
     day = parseInt(day.toString().split(".")[0]);
@@ -96,7 +97,7 @@ Router.post("/", (req, res) => {
         " returning " +
         req.body.returning
     );
-    console.log(analytics);
+
     writeJSON("analytics.json", JSON.stringify(analytics));
   }
 
