bb2976135c2c6cf0263a3ccaa4fb6785e983c34b
DiamonC <88857034+DiamonC@users.noreply.github.com>
2024-11-20 16:29:22 -0600

    improved sidebar on mobile

--- src/lib/components/ui/ServerSkeleNew.svelte
+++ src/lib/components/ui/ServerSkeleNew.svelte
@@ -1,8 +1,10 @@
-<div class="w-[3.75rem] h-[3.75rem] rounded-lg bg-base-300 animate-pulse" />
+<div
+  class="w-[3.75rem] h-[3.75rem] rounded-lg bg-base-300 animate-pulse max-md:hidden"
+/>
 <div class="-mt-1">
   <div class="w-24 h-5 rounded bg-base-300 animate-pulse" />
-  <div class="w-12 h-3 rounded bg-base-300 animate-pulse my-1" />
-  <div class="flex gap-2">
+  <div class="w-12 h-3 rounded bg-base-300 animate-pulse my-1 max-md:hidden" />
+  <div class="flex gap-2 max-md:hidden">
     <div class="w-[3.75rem] h-2 rounded bg-base-300 animate-pulse" />
     <p class="bg-base-300 px-1.5 rounded text-xs animate-pulse"></p>
   </div>
