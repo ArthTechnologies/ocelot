158be993368bc43b798ee3221abd8ec973ace0af
DiamonC <88857034+DiamonC@users.noreply.github.com>
2024-11-19 22:22:58 -0600

    added navbar for mobile

--- src/routes/(login)/+layout.svelte
+++ src/routes/(login)/+layout.svelte
@@ -1,10 +1,14 @@
 <script>
+  import Navbar from "$lib/components/layout/Navbar.svelte";
   import Sidebar from "$lib/components/layout/Sidebar.svelte";
 </script>
 
-<div class="flex bg-base-200 min-h-screen">
-  <div class="w-1/5">
-    <Sidebar></Sidebar>
+<div class="flex max-md:flex-col max-md:items-center bg-base-200 min-h-screen">
+  <div class="w-1/5 max-md:hidden">
+    <Sidebar />
+  </div>
+  <div class="w-full md:hidden">
+    <Navbar />
   </div>
   <div class="m-4 sm:m-6 md:m-8 pb-12 lg:mt-16 w-4/5">
     <slot />
