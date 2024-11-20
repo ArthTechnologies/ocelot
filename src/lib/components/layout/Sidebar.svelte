973b9a069e360d16794f7e108165ebe106c54b1c
DiamonC <88857034+DiamonC@users.noreply.github.com>
2024-11-19 20:52:18 -0600

    completely new design, added sidebar navigation

---
+++ src/lib/components/layout/Sidebar.svelte
@@ -0,0 +1,236 @@
+<script lang="ts">
+  import ServerCardNew from "$lib/components/ui/ServerCardNew.svelte";
+  import UncreatedServerCard from "$lib/components/ui/UncreatedServerCard.svelte";
+  import ServerSkele from "$lib/components/ui/ServerSkele.svelte";
+  import { t, locale, locales } from "$lib/scripts/i18n";
+  import { apiurl, getServers } from "$lib/scripts/req";
+  import { browser, dev } from "$app/environment";
+  import { goto } from "$app/navigation";
+  import { onMount } from "svelte";
+  import { parse } from "path";
+  import { HelpCircle, Languages, ShoppingCart, User } from "lucide-svelte";
+
+  // NOTE: the element that is using one of the theme attributes must be in the DOM on mount
+  let servers: any[] = [];
+  //Example
+
+  var id = 0;
+
+  let noserverlock = false;
+  let amountOfServersForSkeletons = 1;
+
+  let slug = 0;
+  let email: string = "";
+  onMount(() => {
+    if (browser) {
+      update(undefined, false);
+    }
+  });
+  if (browser) {
+    email = localStorage.getItem("accountEmail");
+    amountOfServersForSkeletons = localStorage.getItem("amountOfServers");
+  }
+
+  // getServers and store "amount" given in the response in a variable
+  let promise;
+
+  if (browser) {
+    promise = getServers(email).then((response) => {
+      if (browser) {
+        noserverlock = true;
+        console.log(response);
+        if (response.amount != "undefined") {
+          id = response.amount;
+        }
+        servers = response;
+        if (
+          servers.length == 0 ||
+          !JSON.stringify(servers).includes(":not created yet")
+        ) {
+          console.log("claiming an id...");
+          fetch(apiurl + "server/claimId", {
+            method: "GET",
+            headers: {
+              token: localStorage.getItem("token"),
+              username: localStorage.getItem("accountEmail"),
+            },
+          })
+            .then((res) => res.json())
+            .then((res) => {
+              console.log(res);
+              if (res.msg == "You haven't paid for a server.") {
+                goto("/billing");
+                //this tells the navbar to update the icon that is highligted
+                window.dispatchEvent(new Event("redrict"));
+              }
+              promise = getServers(email).then((response) => {
+                if (browser) {
+                  noserverlock = true;
+                  console.log(response);
+                  if (response.amount != "undefined") {
+                    id = response.amount;
+                  }
+                  servers = response;
+                  let slug2 = window.location.pathname;
+                  if (slug2 == "/") {
+                    goto("/server/" + (10000 + parseInt(servers[0].id)));
+                    update(servers[0].id, false);
+                  }
+                }
+              });
+            });
+        }
+      }
+    });
+  }
+
+  let noserver = false;
+
+  if (id == 0 && noserverlock) {
+    noserver = true;
+  }
+
+  function update(id, redirect) {
+    console.log("updating");
+    if (id == undefined) {
+      setTimeout(() => {
+        slug = window.location.pathname.split("/")[2];
+      }, 10);
+      setTimeout(() => {
+        slug = window.location.pathname.split("/")[2];
+      }, 50);
+      setTimeout(() => {
+        slug = window.location.pathname.split("/")[2];
+      }, 250);
+    } else {
+      slug = 10000 + parseInt(id);
+
+      for (let i = 0; i < servers.length; i++) {
+        if (parseInt(servers[i].id) + 10000 == slug) {
+          console.log(slug);
+          select(servers[i]);
+          if (redirect) {
+            goto("/server/" + slug);
+            //reload
+            setTimeout(() => {
+              window.location.reload();
+            }, 100);
+          }
+        }
+      }
+    }
+  }
+
+  function select(server) {
+    console.log("server");
+
+    localStorage.setItem("serverName", server.name);
+    localStorage.setItem("serverID", server.id);
+    localStorage.setItem("serverSoftware", server.software);
+    localStorage.setItem("serverVersion", server.version);
+    localStorage.setItem("serverCardRedrict", "true");
+    localStorage.setItem("serverWebmap", server.webmap);
+    localStorage.setItem("serverVoicechat", server.voicechat);
+    if (server.subdomain != undefined) {
+      localStorage.setItem("serverSubdomain", server.subdomain);
+    } else {
+      localStorage.removeItem("serverSubdomain");
+    }
+  }
+</script>
+
+<div
+  class="shrink-0 min-h-screen bg-base-100 p-5 flex flex-col items-center justify-between"
+>
+  <div class="flex flex-col items-center w-full">
+    <img src="/images/sitelogo.svg" class="w-40" />
+    <div class="divider"></div>
+    <div class="flex flex-col gap-3 w-full">
+      {#each servers as server}
+        {#if parseInt(server.id) + 10000 == slug}
+          <a
+            id="serverCard{10000 + parseInt(server.id)}"
+            class="primaryGradientStroke pointer-events-none flex gap-2.5 items-center p-3 w-full h-[5.25rem] rounded-lg bg-gradient-to-b from-base-200 to-[#1a2b40] cursor-pointer"
+          >
+            <ServerCardNew {...server} />
+          </a>
+        {:else}
+          <a
+            on:click={() => update(server.id, true)}
+            id="serverCard{10000 + parseInt(server.id)}"
+            class="neutralGradientStroke flex gap-2.5 items-center p-3 w-full h-[5.25rem] rounded-lg bg-base-200 cursor-pointer"
+          >
+            <ServerCardNew {...server} />
+          </a>
+        {/if}
+      {/each}
+    </div>
+  </div>
+  <div class="flex flex-col w-full gap-1">
+    <button class="btn btn-ghost btn-sm flex justify-start">
+      <User size="20" class="mr-2" />Manage Account</button
+    >
+    <button class="btn btn-ghost btn-sm flex justify-start">
+      <ShoppingCart size="20" class="mr-2" />Subscriptions</button
+    >
+
+    <button class="btn btn-ghost btn-sm flex justify-start">
+      <Languages size="20" class="mr-2" />Language</button
+    >
+    <button class="btn btn-ghost btn-sm flex justify-start">
+      <HelpCircle size="20" class="mr-2" />Get Support</button
+    >
+  </div>
+</div>
+
+<style>
+  .primaryGradientStroke {
+    position: relative;
+
+    z-index: 1;
+  }
+
+  .primaryGradientStroke::before {
+    content: "";
+    position: absolute;
+    top: 0px;
+
+    bottom: 0px;
+    left: 0px;
+    right: 0px;
+    border-radius: inherit; /* Inherits button's border-radius */
+    padding: 2.5px; /* Space between button and border */
+    background: linear-gradient(0deg, #135664, #ffffff00, #ffffff00, #ffffff00);
+    -webkit-mask:
+      linear-gradient(#fff 0 0) content-box,
+      linear-gradient(#fff 0 0);
+    -webkit-mask-composite: destination-out;
+    mask-composite: exclude;
+    z-index: -1;
+  }
+
+  .neutralGradientStroke {
+    position: relative;
+
+    z-index: 1;
+  }
+
+  .neutralGradientStroke:hover::before {
+    content: "";
+    position: absolute;
+    top: 0px;
+
+    bottom: 0px;
+    left: 0px;
+    right: 0px;
+    border-radius: inherit; /* Inherits button's border-radius */
+    padding: 2.5px; /* Space between button and border */
+    background: linear-gradient(0deg, #2a354e, #ffffff00, #ffffff00, #ffffff00);
+    -webkit-mask:
+      linear-gradient(#fff 0 0) content-box,
+      linear-gradient(#fff 0 0);
+    -webkit-mask-composite: destination-out;
+    mask-composite: exclude;
+    z-index: -1;
+  }
+</style>
--- src/routes/(login)/+layout.svelte
+++ src/routes/(login)/+layout.svelte
@@ -1,12 +1,12 @@
 <script>
-  import Navbar from "$lib/components/layout/Navbar.svelte";
-  import Footer from "$lib/components/layout/Footer.svelte";
+  import Sidebar from "$lib/components/layout/Sidebar.svelte";
 </script>
 
-<div class="flex flex-col justify-between">
-  <Navbar navType="default" />
-  <div class="m-4 sm:m-6 md:m-8 pb-12 lg:mt-16">
+<div class="flex bg-base-200 min-h-screen">
+  <div class="w-1/5">
+    <Sidebar></Sidebar>
+  </div>
+  <div class="m-4 sm:m-6 md:m-8 pb-12 lg:mt-16 w-4/5">
     <slot />
   </div>
-  <Footer navType="default" />
 </div>
