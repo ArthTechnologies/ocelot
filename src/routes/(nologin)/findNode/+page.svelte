72a96ef8974eed02d647edc5b9a9108cf622e171
DiamonC <88857034+DiamonC@users.noreply.github.com>
2024-12-30 18:22:00 -0600

    added mechanism to connect to an available node

---
+++ src/routes/(nologin)/findNode/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { browser } from "$app/environment";
+    import { goto } from "$app/navigation";
+if (browser) {
+    
+    fetch('https://backend.arthmc.xyz/availableNode')
+        .then(response => response.text())
+        .then(data => {
+            
+            if (data != "null") {
+                //if data doesn't have a / at the end, add it
+                if (data[data.length - 1] != '/') {
+                    data += '/';
+                }
+                localStorage.setItem('userNode', data);
+                goto('/login');
+            }
+        });
+}
+</script>
