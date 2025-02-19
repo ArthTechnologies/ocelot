d1f6d3a39b38d3510bbe2d40a80749df7d29fb77
DiamonC <88857034+DiamonC@users.noreply.github.com>
2025-02-18 21:42:11 -0600

    added special purpose migrate to europe page

---
+++ src/routes/(nologin)/auth/migrate-to-europe/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { browser } from "$app/environment";
+    import { goto } from "$app/navigation";
+
+    function migrate() {
+        if (browser) {
+            localStorage.setItem("userNode", "https://eu-germany-1.arthmc.xyz/");
+            goto("/");
+           
+        }
+    }
+</script>
+
+
+<button class="btn mt-12 btn-neutral" on:click={migrate}>Migrate to europe</button>
+<p>Only use this if you were sent this link by Arth Hosting staff.</p>
