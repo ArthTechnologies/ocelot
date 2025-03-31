a75523ebbecf4c7123fb83ce4b534fbc7eef1223
DiamonC <88857034+DiamonC@users.noreply.github.com>
2025-03-31 16:54:28 -0500

    made chooseLocation page apply to all login types

--- src/routes/(nologin)/login/+page.svelte
+++ src/routes/(nologin)/login/+page.svelte
@@ -6,12 +6,21 @@
 
   import { t, locale, locales } from "$lib/scripts/i18n";
   import { getSettings } from "$lib/scripts/req";
-  import { PersonStanding, ShoppingCart, Box } from "lucide-svelte";
+  import { PersonStanding, ShoppingCart, Box, ArrowLeft } from "lucide-svelte";
   let address;
   let plan = undefined;
+  let locationName = "Loading...";  
   getSettings();
 
   if (browser) {
+    locationName = localStorage.getItem("userNode");
+    if (locationName.includes("https://")) {
+      locationName = locationName.split("https://")[1];
+      locationName = locationName.split(".")[0];
+    
+    } else {
+      goto("/auth/chooseLocation");	  
+    }
     plan = document.location.search.split("plan=")[1];
     address = window.location.host;
     //add in http or https depending on the protocol
@@ -51,28 +60,10 @@
     class="relative bg-base-100 rounded-xl shadow-xl px-5 md:px-8 xl:px-12 flex flex-col items-center max-sm:w-[95%]"
   >
     <div
-      class="max-md:hidden mt-5 px-4 py-2 bg-primary rounded-full text-gray-200 font-semibold text-sm flex items-center gap-2"
+      class="max-md:hidden mt-5 px-4 py-1 bg-neutral rounded-full font-semibold text-sm flex items-center gap-1"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="18"
-        height="18"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="lucide lucide-pickaxe"
-        ><path d="M14.531 12.469 6.619 20.38a1 1 0 1 1-3-3l7.912-7.912" /><path
-          d="M15.686 4.314A12.5 12.5 0 0 0 5.461 2.958 1 1 0 0 0 5.58 4.71a22 22 0 0 1 6.318 3.393"
-        /><path
-          d="M17.7 3.7a1 1 0 0 0-1.4 0l-4.6 4.6a1 1 0 0 0 0 1.4l2.6 2.6a1 1 0 0 0 1.4 0l4.6-4.6a1 1 0 0 0 0-1.4z"
-        /><path
-          d="M19.686 8.314a12.501 12.501 0 0 1 1.356 10.225 1 1 0 0 1-1.751-.119 22 22 0 0 0-3.393-6.319"
-        /></svg
-      >
-      {$t("auth.welcomeBack")}
+      
+      <a href="/auth/chooseLocation" class="btn btn-xs btn-circle btn-ghost -ml-2"><ArrowLeft size=18 /></a>{locationName}
     </div>
     <!-- Signup Section-->
     <div class="p-4 md:p-6 md:w-96 rounded-xl">
