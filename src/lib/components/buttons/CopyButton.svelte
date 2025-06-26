61cd2eccd1557f00c42b988fabb400fbc7803fd4
DiamonC <88857034+DiamonC@users.noreply.github.com>
2025-06-25 22:35:45 -0500

    added inter font, consolidated server info card

---
+++ src/lib/components/buttons/CopyButton.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { alert } from "$lib/scripts/utils";
+    import { CopyIcon } from "lucide-svelte";
+    export let text = "";
+</script>
+<button
+    class="btn btn-ghost btn-sm btn-circle"
+    on:click={() => {
+        navigator.clipboard.writeText(text);
+        alert("Copied to clipboard", "success");
+    }}
+    aria-label="Copy to clipboard"
+>
+    <CopyIcon size=16 />
+</button>
