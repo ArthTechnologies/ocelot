d2b7462f4b5aeaccc6584ea5d85d9b321b741a9d
DiamonC <88857034+DiamonC@users.noreply.github.com>
2025-06-28 23:43:01 -0500

    added offline players to playerlist

--- src/lib/components/pages/server/PlayerList.svelte
+++ src/lib/components/pages/server/PlayerList.svelte
@@ -3,7 +3,8 @@
     import { apiurl } from "$lib/scripts/req";
     
     export let id;
-    let players = [];
+    let playersOnline = [];
+    let playersOffline = [];
     let loadingStates = {};
     let headCache = new Set(); // Tracks cached player heads
 
@@ -23,14 +24,38 @@
             });
             
             const data = await response.json();
-            players = data;
+            playersOnline = data.playersOnline || [];
             
             // Initialize only new players not in cache
-            data.forEach(player => {
+            playersOnline.forEach(player => {
                 if (!(player.uuid in loadingStates)) {
                     loadingStates[player.uuid] = !headCache.has(player.uuid);
                 }
             });
+            let off = [];
+            for (let i in data.allPlayers) {
+        
+                const player = data.allPlayers[i];
+                // if the player is not online, add them to offline list
+                if (!playersOnline.some(p => p.uuid === player.uuid)) {
+                    
+                    if (!playersOffline.some(p => p.uuid === player.uuid)) {
+                               
+                        off.push(player);
+                    }
+                }
+            }
+     
+            playersOffline = off;
+
+            // Initialize loading states for offline players
+            playersOffline.forEach(player => {
+                if (!(player.uuid in loadingStates)) {
+                    loadingStates[player.uuid] = !headCache.has(player.uuid);
+                }
+            }); 
+
+            
         } catch (error) {
             console.error("Error fetching players:", error);
         }
@@ -46,26 +71,44 @@
 <div class="bg-base-300 w-full shadow-xl rounded-xl px-4 py-3 neutralGradientStroke">
     <p class="font-bold font-ubuntu text-gray-100 mb-3">Players</p>
     <ul class="flex flex-col gap-1.5">
-        {#each players as player (player.uuid)}
-            <li class="text-gray-200 bg-base-100 w-full p-2 px-2.5 rounded-xl font-mono flex items-center">
+        {#each playersOnline as player (player.uuid)}
+            <li class="text-gray-200 bg-base-100 w-full p-2 px-2.5 rounded-xl font-mono text-sm flex items-center">
                 <div class="relative mr-2">
                     {#if loadingStates[player.uuid]}
-                        <div class="w-8 h-8 bg-gray-600 rounded animate-pulse" />
+                        <div class="w-6 h-6 bg-gray-600 rounded animate-pulse" />
                     {/if}
                     
               
                  <img
                         src={`https://mc-heads.net/avatar/${player.uuid}`}
                         alt={player.name + "'s head"}
-                        class={`w-8 h-8 rounded ${loadingStates[player.uuid] ? 'hidden' : 'block'}`}
+                        class={`w-6 h-6 rounded ${loadingStates[player.uuid] ? 'hidden' : 'block'}`}
                         on:load={() => handleImageLoad(player.uuid)}
                     />   {#if player.name.split("")[0] == "."}
-                    <img class="w-4 h-4 rounded absolute -bottom-1 -right-1 shadow-xl/30" src="/images/bedrock.webp" alt="Bedrock" />
+                    <img class="w-3 h-3 rounded absolute -bottom-1 -right-1 shadow-xl/30" src="/images/bedrock.webp" alt="Bedrock" />
                    {/if}
             
                 </div>
                 {player.name}
             </li>
         {/each}
+        {#each playersOffline as player (player.uuid)}
+            <li class="text-gray-400 bg-base-100 w-full p-2 px-2.5 rounded-xl font-mono text-sm flex items-center">
+                <div class="relative mr-2">
+                    {#if loadingStates[player.uuid]}
+                        <div class="w-6 h-6 bg-gray-600 rounded animate-pulse" />
+                    {/if}
+                    
+                    <img
+                        src={`https://mc-heads.net/avatar/${player.uuid}`}
+                        alt={player.name + "'s head"}
+                        class={`w-6 h-6 grayscale opacity-75 rounded ${loadingStates[player.uuid] ? 'hidden' : 'block'}`}
+                        on:load={() => handleImageLoad(player.uuid)}
+                    />
+                </div>
+                {player.name}
+            </li>
+        {/each}
+
     </ul>
 </div>
