e131e350e4c6fdf6a62d8c4507769e5e2c813c72
DiamonC <88857034+DiamonC@users.noreply.github.com>
2025-07-13 13:13:00 -0500

    redesigned ram cpu charts and moved to top right

--- src/routes/(login)/server/[slug]/+page.svelte
+++ src/routes/(login)/server/[slug]/+page.svelte
@@ -67,8 +67,8 @@
   import Plugins from "$lib/components/pages/server/Plugins.svelte";
   import Mods from "$lib/components/pages/server/Mods.svelte";
   import Datapacks from "$lib/components/pages/server/Datapacks.svelte";
-  import MemoryChart from "$lib/components/pages/dashboard/MemoryChart.svelte";
-  import CpuUsageChart from "$lib/components/pages/dashboard/CpuUsageChart.svelte";
+  import MemoryChart from "$lib/components/pages/server/charts/MemoryChart.svelte";
+  import CpuUsageChart from "$lib/components/pages/server/charts/CpuUsageChart.svelte";
   import Backups from "$lib/components/pages/server/Backups.svelte";
     import CopyButton from "$lib/components/buttons/CopyButton.svelte";
     import ServerInfo from "$lib/components/pages/server/ServerInfo.svelte";
@@ -485,19 +485,35 @@
       </div>
     </div>
 
-    <div class="flex flex-col gap-1.5 hidden md:flex">
-      <div
-        class="flex bg-neutral px-2 p-1.5 rounded-lg items-center text-sm font-bold gap-1 h-fit"
-      >
-        <MemoryStick size="16" />
-        {ramUsage}
-      </div>
-      <div
-        class="flex bg-neutral px-2 p-1.5 rounded-lg items-center text-sm font-bold gap-1 h-fit"
-      >
-        <Users size="16" />
-        {players}/{maxPlayers} Players
-      </div>
+    <div class="flex gap-1.5 hidden md:flex scale-95 w-[20rem]">
+         
+
+          {#await memoryReq}
+            <div class="flex gap-12">
+              <div
+                class="w-[12rem] h-[8.3rem] bg-gradient-to-t from-[#152036] to-[#2c2a27] rounded-xl"
+              ></div>
+              <div
+                class="w-[12rem] h-[8.3rem] bg-gradient-to-t from-[#152036] to-[#152436] rounded-xl"
+              ></div>
+            </div>
+          {:then}
+            {#if memoryStats != undefined && memoryStats.length > 0}
+              <MemoryChart performance={memoryStats} type="2" />
+              <CpuUsageChart performance={memoryStats} type="2" />
+            {/if}
+          {:catch}
+            <div class="flex gap-12">
+              <div
+                class="w-[12rem] h-[8.3rem] bg-gradient-to-t from-[#152036] to-[#2c2a27] rounded-xl"
+              ></div>
+              <div
+                class="w-[12rem] h-[8.3rem] bg-gradient-to-t from-[#152036] to-[#152436] rounded-xl"
+              ></div>
+            </div>
+          {/await}
+      
+ 
     </div>
   </div>
   <!-- End Top Section-->
@@ -574,36 +590,10 @@
     >
       <div class="space-y-5 w-full">
     <ServerInfo {name} {address} {port} {subdomain} {modded} />
+
     <PlayerList {id}/>
       </div>
-      <div class="flex w-full">
-        <div class="scale-95 w-full -ml-1.5">
-          {#await memoryReq}
-            <div class="flex gap-12">
-              <div
-                class="w-[12rem] h-[8.3rem] bg-gradient-to-t from-[#152036] to-[#2c2a27] rounded-xl"
-              ></div>
-              <div
-                class="w-[12rem] h-[8.3rem] bg-gradient-to-t from-[#152036] to-[#152436] rounded-xl"
-              ></div>
-            </div>
-          {:then}
-            {#if memoryStats != undefined && memoryStats.length > 0}
-              <MemoryChart performance={memoryStats} type="2" />
-              <CpuUsageChart performance={memoryStats} type="2" />
-            {/if}
-          {:catch}
-            <div class="flex gap-12">
-              <div
-                class="w-[12rem] h-[8.3rem] bg-gradient-to-t from-[#152036] to-[#2c2a27] rounded-xl"
-              ></div>
-              <div
-                class="w-[12rem] h-[8.3rem] bg-gradient-to-t from-[#152036] to-[#152436] rounded-xl"
-              ></div>
-            </div>
-          {/await}
-        </div>
-      </div>
+
 
       {#if dynmap}
         <div
