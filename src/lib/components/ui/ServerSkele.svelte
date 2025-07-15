ad3f5f0fe8dfeb4851c4f53dc6e52d7c7c47be72
DiamonC <88857034+DiamonC@users.noreply.github.com>
2025-07-15 16:29:58 -0500

    updated look of servercard skeleton

--- src/lib/components/ui/ServerSkele.svelte
+++ src/lib/components/ui/ServerSkele.svelte
@@ -1,12 +1,12 @@
 <div
-  class="w-[3.75rem] h-[3.75rem] rounded-lg bg-base-300 animate-pulse max-md:hidden"
+  class="w-[3.75rem] h-[3.75rem] rounded-lg bg-gray-800 skeleton max-md:hidden"
 />
 <div class="-mt-1">
-  <div class="w-24 h-5 rounded bg-base-300 animate-pulse" />
-  <div class="w-12 h-3 rounded bg-base-300 animate-pulse my-1 max-md:hidden" />
+  <div class="w-24 h-5 rounded bg-gray-800 skeleton" />
+  <div class="w-12 h-3 rounded bg-gray-800 skeleton my-1 max-md:hidden" />
   <div class="flex gap-2 max-md:hidden">
-    <div class="w-[3.75rem] h-2 rounded bg-base-300 animate-pulse" />
-    <p class="bg-base-300 px-1.5 rounded text-xs animate-pulse"></p>
+    <div class="w-[3.75rem] h-2 rounded bg-gray-800 skeleton" />
+    <p class="bg-gray-800 px-1.5 rounded text-xs skeleton"></p>
   </div>
 </div>
 
