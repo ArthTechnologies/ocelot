0a690e1cbfbb0ce2090cf411788ab6b95f83bd08
DiamonC <88857034+DiamonC@users.noreply.github.com>
2025-07-15 19:23:43 -0500

    improved plugin/mod/datapack skeleton loader

--- src/lib/components/ui/ManagePluginSkele.svelte
+++ src/lib/components/ui/ManagePluginSkele.svelte
@@ -4,7 +4,7 @@
 
 <div class="flex flex-col">
   <div
-    class="flex items-center justify-between bg-base-300 rounded-t-lg w-full h-[2.75rem] pr-3 py-2 pl-2.5 space-x-1"
+    class="flex items-center justify-between bg-base-300 rounded-lg w-full h-16 pr-3 py-2 pl-2.5 space-x-1"
   >
     <div class="flex items-center space-x-1">
       <div class="bg-slate-700 rounded-lg w-[13rem] h-[1.5rem] animate-pulse" />
@@ -22,13 +22,5 @@
       </div>
     </div>
   </div>
-  <div
-    class="h-[4.5rem] bg-base-200 rounded-b-lg px-3 pt-[1.125rem] pb-[.75rem] flex flex-col justify-between"
-  >
-    <div class="flex space-x-1 items-end">
-      <div class="bg-slate-700 rounded-lg w-[5rem] h-4 animate-pulse" />
-      <div class="bg-slate-700 rounded-lg w-[6rem] h-3 animate-pulse" />
-    </div>
-    <div class="bg-slate-700 rounded-lg w-[13rem] h-3.5 animate-pulse" />
-  </div>
+
 </div>
