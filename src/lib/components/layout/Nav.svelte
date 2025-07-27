f7742a0da381cc9e094c0aad4df4360aa73bbbb7
DiamonC <88857034+DiamonC@users.noreply.github.com>
2025-07-27 15:22:26 -0500

    fixed errors with navbar

--- src/lib/components/layout/Nav.svelte
+++ src/lib/components/layout/Nav.svelte
@@ -27,7 +27,7 @@
   let servers: any[] = [];
   //Example
 
-  var id = 0;
+  var id = undefined;
 
   let devMode = false;
   let providerMode = true;
@@ -40,7 +40,7 @@
   onMount(() => {
     if (browser) {
 
-      update( false);
+      update(undefined, false);
     }
   });
   if (browser) {
@@ -62,10 +62,6 @@
 
      loadServers();
      window.addEventListener("redrict", loadServers);
-     window.addEventListener("redrict", () => {
-       update(true)
-     });
-   
   }
 
   function loadServers() {
@@ -143,7 +139,7 @@
              
                       goto("/server/" + (10000 + parseInt(servers[0].id)));
                     }
-                    update( false);
+                    update(servers[0].id, false);
                   }
                 }
               });
@@ -169,6 +165,8 @@
       console.log("TTTTTTT" + window.location.search.split("?id=")[1]);
       slug = parseInt(window.location.search.split("?id=")[1]);
       console.log("slug is " + slug);
+     } else {
+      slug = 0;
      }
    }
   let noserver = false;
@@ -177,10 +175,27 @@
     noserver = true;
   }
 
-  function update(redirect) {
+  function update(id, redirect) {
     console.log("updating");
-  
-      getSlug();
+    id = parseInt(id);
+   
+       
+    
+
+      if (redirect) {
+      for (let i = 0; i < servers.length; i++) {
+        let newslug = 10000 + parseInt(id);
+        if (parseInt(servers[i].id) + 10000 == newslug) {
+          console.log("selecting server " + servers[i].name  + " with id " + servers[i].id + " " + id);
+          select(servers[i]);
+           slug = newslug;
+
+            window.location.href = "/server/" + newslug;
+
+          }
+        }
+      } else {
+ getSlug();
       setTimeout(() => {
         getSlug();
       }, 50);
@@ -188,18 +203,7 @@
         getSlug();
    
       }, 250);
-
-      
-
-      for (let i = 0; i < servers.length; i++) {
-        if (parseInt(servers[i].id) + 10000 == slug) {
-          console.log(slug);
-          select(servers[i]);
-          if (redirect) {
-            window.location.href = "/server/" + slug;
-
-          }
-        }
+      console.log("SLUG IS " + slug)
       }
     
   }
@@ -264,6 +268,7 @@
 {#if parseInt(server.split(":")[0]) == -1}
 Invalid Account
 {:else}
+
 {#if parseInt(server.split(":")[0]) == slug}
 <a
   on:click={() => createServer(parseInt(server.split(":")[0]))}
@@ -300,7 +305,7 @@ Invalid Account
 </a>
 {:else}
 <a
-  on:click={() => update( true)}
+  on:click={() => update(server.id, true)}
   id="serverCard{10000 + parseInt(server.id)}"
   class="neutralGradientStrokeB flex gap-2.5 items-center p-4 w-14 sm:w-32 truncate md:w-full md:h-[5.5rem] rounded-xl bg-base-200 cursor-pointer"
 >
@@ -313,11 +318,11 @@ Invalid Account
     </div>
   </div>
   <div class="max-md:hidden flex flex-col w-full gap-1">
-    <a on:click={()=>{update( false)}} href="/account" class="font-ubuntu btn btn-ghost btn-ms flex justify-start hover:text-primary">
+    <a on:click={()=>{update(undefined, false)}} href="/account" class="font-ubuntu btn btn-ghost btn-ms flex justify-start hover:text-primary">
       <User size="20" />Account</a
     >
 {#if providerMode}
-    <a on:click={()=>{update( false)}} href="/billing" class="font-ubuntu btn btn-ghost btn-ms flex justify-start hover:text-primary">
+    <a on:click={()=>{update(undefined, false)}} href="/billing" class="font-ubuntu btn btn-ghost btn-ms flex justify-start hover:text-primary">
       <ShoppingCart size="20" />Subscriptions</a
     >
     {/if}
@@ -347,9 +352,9 @@ Invalid Account
         tabindex="0"
         class="dropdown-content menu bg-base-300 rounded-box z-[1] w-52 p-2 shadow-xl"
       >
-        <li><a on:click={()=>{update( false)}} href="/account">Account</a></li>
+        <li><a on:click={()=>{update(undefined, false)}} href="/account">Account</a></li>
         {#if providerMode}
-        <li><a on:click={()=>{update( false)}} href="/billing">Subscriptions</a></li>
+        <li><a on:click={()=>{update(undefined, false)}} href="/billing">Subscriptions</a></li>
         {/if}
 <li>        <a onclick="modal_language.showModal()">
   Language</a
