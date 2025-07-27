b6b6103c6d8fb5e481d5d3752bf50e22dc6cb46b
DiamonC <88857034+DiamonC@users.noreply.github.com>
2025-07-27 14:16:58 -0500

    vastly improved mod skeleton loaders

--- src/lib/components/pages/server/Mods.svelte
+++ src/lib/components/pages/server/Mods.svelte
@@ -283,8 +283,11 @@
 
 
     <div class="space-y-2">
+       
       {#await promise}
-        <ManagePluginSkele />
+        {#each Array(8) as _}
+          <ManagePluginSkele />
+        {/each}
       {:then}
         {#each res.mods as mod}
           {#if mod.name != "CFMod" && (mod.name != mod.id || mod.id == undefined)}
