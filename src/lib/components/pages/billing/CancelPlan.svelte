8208febd245fb66a513728ce7b40a81b8f6330ad
DiamonC <88857034+DiamonC@users.noreply.github.com>
2025-08-01 15:06:30 -0500

    added WIP new billing page

---
+++ src/lib/components/pages/billing/CancelPlan.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { ArrowDownUp, X } from "lucide-svelte";
+    export let subscription = {};
+    
+</script>
+   <label for="delete" class="btn btn-error btn-sm" 
+                                            >
+                                                <X size=16 class="mr-1"/>
+                                                Cancel
+</label>
+<!-- Put this part before </body> tag -->
+<input type="checkbox" id="delete" class="modal-toggle" />
+<div class="modal" style="margin:0rem;">
+  <div class="modal-box bg-opacity-95 backdrop-blur relative">
+    <label
+      for="delete"
+      class="btn btn-neutral btn-sm btn-circle absolute right-2 top-2">✕</label
+    ></div></div>
