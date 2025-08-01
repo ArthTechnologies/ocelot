22f092809f2e72a68df942364e5f5385a8ab44bc
DiamonC <88857034+DiamonC@users.noreply.github.com>
2025-08-01 15:58:57 -0500

    added sub cancelling to wip new billing page

--- src/lib/components/pages/billing/CancelPlan.svelte
+++ src/lib/components/pages/billing/CancelPlan.svelte
@@ -1,6 +1,35 @@
 <script>
-    import { ArrowDownUp, X } from "lucide-svelte";
+    import { t } from "$lib/scripts/i18n";
+    import { apiurl } from "$lib/scripts/req";
+    import { alert } from "$lib/scripts/utils";
+    import { AlertTriangle, ArrowDownUp, Loader, X } from "lucide-svelte";
     export let subscription = {};
+    let loading = false;
+
+    function cancel() {
+        loading=true;
+        fetch(apiurl+"x/lapis/cancel/"+subscription.id+"?mode=stripe", {
+      method: "POST",
+      headers: {
+        username: localStorage.getItem("accountEmail"),
+        token: localStorage.getItem("token"),
+      },
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        if (json.status == "canceled") {
+            alert("Subscription succesfully canceled.", "success")
+            const event = new CustomEvent("refresh");
+                  document.dispatchEvent(event);
+        } else {
+            alert("Error canceling subscription. Please contant support.")
+        }
+        
+        
+      });
+
+        
+    }
     
 </script>
    <label for="delete" class="btn btn-error btn-sm" 
@@ -15,4 +44,22 @@
     <label
       for="delete"
       class="btn btn-neutral btn-sm btn-circle absolute right-2 top-2">✕</label
-    ></div></div>
+    >
+    <h3 class="text-lg font-bold">{$t("server.delete.title")}</h3>
+    <div
+      class="bg-warning w-86  rounded-lg text-black p-2 flex items-center space-x-2 mt-2"
+    >
+      <AlertTriangle />
+      <span class="text-sm">You will be unable to use your server, and your data may be deleted afted 7 days.</span>
+
+
+    </div>
+
+
+      <button id="delButton" class="btn btn-error mt-8" on:click={cancel}>
+        {#if loading}
+          <Loader class="mr-1.5 animate-spin" />Cancelling
+        {:else}
+          Cancel{/if}</button
+      >
+</div></div>
