39de1a4fbbf7052a80cf882a52f2fa583576b155
DiamonC <88857034+DiamonC@users.noreply.github.com>
2025-08-06 16:05:17 -0500

    wip refferal system

---
+++ src/routes/(login)/referrals/+page.svelte
@@ -0,0 +1,90 @@
+<script>
+    import { browser } from "$app/environment";
+    import { t } from "$lib/scripts/i18n";
+    import { apiurl } from "$lib/scripts/req";
+    import { alert } from "$lib/scripts/utils";
+    let email = "xxxxx";
+    let coupon;
+    let used = false;
+    if (browser) {
+        email = localStorage.getItem("email");
+        coupon = "arthref_" + email.split('')[3]+ email.split('')[2] + email.split('')[4] + email.split('')[1] + email.split('')[5];
+        fetch(apiurl+"referrals/referred_coupon?email="+email, {
+      method: "GET",
+      headers: {
+        username: localStorage.getItem("accountEmail"),
+        token: localStorage.getItem("token"),
+      },
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        if (!json.includes("coupon_")) {
+            alert("Issue setting up referral coupons.")
+        }
+      });
+    
+
+            fetch(apiurl+"referrals/referrer_coupon/"+coupon, {
+      method: "GET",
+      headers: {
+        username: localStorage.getItem("accountEmail"),
+        token: localStorage.getItem("token"),
+      },
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        used = json.used});
+      }
+
+</script>
+<div class="flex place-content-center">
+  <div class="flex flex-col grow max-w-[55rem] space-y-6">
+    <div class="flex flex-col items-center">
+      <h1 class="divider px-10 text-3xl font-semibold">Referral Program</h1>
+       <table class="table bg-neutral bg-opacity-50 mt-8  w-[20rem]">
+    <!-- head -->
+    <thead class="">
+      <tr>
+       
+        <th>Bill for next month</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <!-- row 1 -->
+      <tr>
+        <td>Current charge</td>
+        <td>$7.99</td>
+      </tr>
+
+      <!-- row 3 -->
+      <tr>
+        <td>Charge if you refer 1 person</td>
+        <td class="font-bold">$4.99</td>
+      </tr>
+          
+    </tbody>
+  </table>
+      <div class="card bg-base-100 shadow-lg transition-shadow mt-4 w-[20rem]">
+                            <div class="card-body">
+                                <div class="flex flex-col gap-4">
+                                    <p class="text-xl font-bold text-white">
+                                        Step 1
+                                    </p>
+                                       <p class="">
+                                        Have a friend signup with the code <code>arthref_{email.split('')[3]+ email.split('')[2] + email.split('')[4] + email.split('')[1] + email.split('')[5]}</code>
+                                    </p>
+                                              <p class="text-xl font-bold text-white">
+                                        Step 2
+                                    </p>
+                                       <p class="">
+                                    {#if !used}
+                                        It looks like your friend hasn't paid for a subscription using your code yet. Come back to this page once they have.
+                                        {/if}
+                                    </p>
+                                    </div>
+                                    </div>
+      </div>
+      </div>
+      </div>
+      </div>
