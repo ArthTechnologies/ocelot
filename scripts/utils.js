f431b2e06a286cae0cae7c34f76016488c28c4c6
DiamonC <88857034+DiamonC@users.noreply.github.com>
2025-10-05 22:20:28 -0500

    Update utils.js

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,20 +1,28 @@
 const fs = require("fs");
+function writeJSON(file, json) {
+  if (file.includes("accounts/")) {
+    console.log("WRITING TO " + file);
+    console.log(json);
+  }
+  try {
+    fs.writeFileSync(file, JSON.stringify(json, null, 2));
+  } catch (error) {
+    console.log("error writing json for " + file, error);
+  }
+}
+
 function readJSON(file) {
   let json = {};
   try {
-    json = JSON.parse(fs.readFileSync(file, "utf8").trim());
+    if (fs.existsSync(file)) {
+      json = JSON.parse(fs.readFileSync(file, "utf8"));
+    } else if (!file.includes("servers/") && !file.includes("accounts/")) {
+      console.log(file + " does not exist.");
+    }
   } catch (error) {
     console.log("error parsing json for " + file, error);
   }
   return json;
-}
-
-function writeJSON(file, json) {
-  try {
-    fs.writeFileSync(file, json, "utf8");
-  } catch (error) {
-    console.log("error writing json for " + file, error);
-  }
 }
 
 module.exports = { readJSON, writeJSON };
