cb9b25d1bc14ccb3d2d4fad886169c1719b51be4
DiamonC <diamonc@proton.me>
2025-10-06 15:27:17 -0500

    removed debug log

--- routes/analytics.js
+++ routes/analytics.js
@@ -18,15 +18,14 @@ Router.post("/", (req, res) => {
     day = parseInt(day.toString().split(".")[0]);
 
     let analytics = readJSON("analytics.json");
-        console.log(typeof analytics);
-        console.log(analytics);
+
     //if analytics are stringified, parse them
     if (typeof analytics == "string") {
       analytics = JSON.parse(analytics);
     }
-        console.log(analytics);
+
     analytics.day = day;
-            console.log("userAgent1: " + req.body.userAgent);
+            
 
     if (analytics.days[day] == undefined) {
       analytics.days[day] = {};
