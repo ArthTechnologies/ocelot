f7021cdd357914ec125441517e308a41b6e2f3b2
DiamonC <88857034+DiamonC@users.noreply.github.com>
2025-11-21 15:12:41 -0600

    added tracking of referrers

--- src/routes/(nologin)/signup/+page.svelte
+++ src/routes/(nologin)/signup/+page.svelte
@@ -7,8 +7,10 @@
     const url = new URL(window.location.href);
     const plan = url.searchParams.get("plan");
     const quarterly = url.searchParams.get("quarterly");
+    const referrer = url.searchParams.get("referrer");
     localStorage.setItem("plan", plan);
     localStorage.setItem("quarterly", quarterly);
+    localStorage.setItem("referrer", referrer);
       goto("/signup/location");
     }
   </script>
