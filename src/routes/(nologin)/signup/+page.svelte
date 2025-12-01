cfc2ac12f18d2636b0097488e57a46d01f1065c7
DiamonC <88857034+DiamonC@users.noreply.github.com>
2025-12-01 16:31:18 -0600

    added campaign name saving

--- src/routes/(nologin)/signup/+page.svelte
+++ src/routes/(nologin)/signup/+page.svelte
@@ -8,9 +8,11 @@
     const plan = url.searchParams.get("plan");
     const quarterly = url.searchParams.get("quarterly");
     const referrer = url.searchParams.get("referrer");
+    const campaign_name = url.searchParams.get("campaign_name");
     localStorage.setItem("plan", plan);
     localStorage.setItem("quarterly", quarterly);
     localStorage.setItem("referrer", referrer);
+    localStorage.setItem("campaign_name", campaign_name);
       goto("/signup/location");
     }
   </script>
