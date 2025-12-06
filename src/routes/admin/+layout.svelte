185d161cc4e4cdec8f8437f75b5ed62d64a3e5af
DiamonC <88857034+DiamonC@users.noreply.github.com>
2025-12-06 17:14:28 -0600

    added admin dashboard to see system tasks and server subscriptions.

---
+++ src/routes/admin/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Nav from "$lib/components/layout/Nav.svelte";
+</script>
+
+<div class="flex min-h-screen">
+  <Nav />
+  <main class="flex-1 md:ml-[22%] bg-base-100">
+    <slot />
+  </main>
+</div>
