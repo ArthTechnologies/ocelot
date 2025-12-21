c8cba3be7864685f3461a31ac6f284e1a9c7e7c2
DiamonC <88857034+DiamonC@users.noreply.github.com>
2025-12-20 22:33:00 -0600

    users are now asked where they heard about arth hosting

--- src/routes/(login)/subscription-success/+page.svelte
+++ src/routes/(login)/subscription-success/+page.svelte
@@ -1,21 +1,13 @@
 <script>
-    import { apiurl } from "$lib/scripts/req";
+  import { apiurl } from "$lib/scripts/req";
+  import { goto } from "$app/navigation";
   import { onMount } from "svelte";
-  import { alert } from "$lib/scripts/utils";
-
 
   onMount(async () => {
     let reservedId = localStorage.getItem("reservedId");
-    let btest = false;
-    btest = localStorage.getItem("btest");
-    let referrer = localStorage.getItem("referrer") || "none";
-    let campaign_name = localStorage.getItem("campaign_name") || "none";
-    fetch("https://ocelot.arthmc.xyz/analytics/sale?btest="+btest+"&referrer="+referrer+"&campaign_name="+campaign_name, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      }
-    });
+
+
+
     fetch(apiurl + "server/claim/" + reservedId, {
       method: "GET",
     })
@@ -25,20 +17,8 @@
           //alert(data.msg, "error");
         }
       });
-  });
-  </script>
 
-<div class="flex justify-center hero bg-base-100 rounded-xl">
-  <div class="flex flex-col hero-content">
-    <div class="space-x-10">
-      <p class="text-left text-2xl">
-        Almost Done! All you have to do now is create a server.
-      </p>
-    </div>
-    <a href="/newserver">
-      <button class="btn btn-secondary btn-outline">
-        Create New Server
-      </button></a
-    >
-  </div>
-</div>
+    // Redirect to newserver with fromSubscription flag
+    goto("/newserver?fromSubscription=true");
+  });
+</script>
