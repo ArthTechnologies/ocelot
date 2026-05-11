7ff63d7c3f5a5d5d07ea26c221fc60b022e85440
DiamonC <diamonc@proton.me>
2026-05-11 16:13:58 -0500

    fixed java version issue

--- quartz/scripts/mc.js
+++ quartz/scripts/mc.js
@@ -319,8 +319,8 @@ function run(
     }
     let javaVer = "8";
     //this selects the correct version of java for the minecraft version
-    // Year-based versions (26.x, 27.x, …) always need Java 21
-    if (parseInt(version.split(".")[0]) >= 2 && !version.startsWith("1.")) javaVer = "21";
+    // Year-based versions (26.x, 27.x, …) always need Java 25
+    if (parseInt(version.split(".")[0]) >= 2 && !version.startsWith("1.")) javaVer = "25";
     else if (parseInt(version.split(".")[1]) >= 20) javaVer = "21";
     else if (version.includes("1.19")) javaVer = "21";
     else if (version.includes("1.18")) javaVer = "17";
@@ -328,7 +328,7 @@ function run(
     if (software == "velocity") javaVer = "17";
 
     if (software == "snapshot") {
-      javaVer = "21";
+      javaVer = "25";
     }
 
     if (server.javaVersion != undefined && !isNaN(server.javaVersion)) {
