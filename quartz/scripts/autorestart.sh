eb5474582dbcbc079ddfc76218ae1a632f48b224
DiamonC <diamonc@proton.me>
2026-05-12 11:21:30 -0500

    startup scripts fixes

--- quartz/scripts/autorestart.sh
+++ quartz/scripts/autorestart.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Load nvm if present (handles nodes where node is installed via nvm rather than apt)
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
 while true; do
    node run
 
